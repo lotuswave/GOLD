db35a2d3b40b78725d0908cb6794ca38f2269fe7
skatipelli <skatipelli@CPC-skatipel-9W.helenoftroy.com>
2022-08-09 19:11:48 +0530

    HydroFlask Updated Test Case names commit

---
+++ src/test/java/TestExecute/Hydroflask/unitTestcase/Test_DGLD_HF_UT_CheckoutShippingStep_038_AdditionalPath_Unauthenticated_UserEntersEmailAssociatedWithAnAccount_ForgotPassword.java
@@ -0,0 +1,54 @@
+package TestExecute.Hydroflask.unitTestcase;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import TestComponent.Hydroflask.HydroHelper;
+import TestLib.Common;
+import TestLib.Login;
+
+public class Test_DGLD_HF_UT_CheckoutShippingStep_038_AdditionalPath_Unauthenticated_UserEntersEmailAssociatedWithAnAccount_ForgotPassword {
+	String datafile = "Hydroflask//HydroTestData.xlsx";	
+	HydroHelper Hydro=new HydroHelper(datafile);
+	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
+	public void UserEntersEmailAssociatedWithAnAccount_ForgotPassword () throws Exception {
+
+		try {
+			Hydro.verifingHomePage();
+			Hydro.headerlinks("Accessories");
+			Hydro.addtocart("Product");
+			Hydro.minicart_viewcart();
+			Hydro.minicart_Checkout();
+	       Hydro.validate_ExistingUser_Login_Checkoutpage("invalidpassword");
+	       Hydro.Validate_Signin_Checkoutpage();
+	       Hydro.forgotpassword();
+		}
+		catch (Exception e) {
+			
+			Assert.fail(e.getMessage(), e);
+		} 
+	}
+	
+	
+	
+	@AfterTest
+	public void clearBrowser()
+	{
+		Common.closeAll();
+	}
+	
+	
+	@BeforeTest
+	  public void startTest() throws Exception {
+		System.setProperty("configFile", "Hydroflask\\config.properties");
+		
+
+		  Login.signIn();
+		 
+		  
+	  }
+
+
+}
