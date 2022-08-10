b20245dd1985c4c3487cb73dcaa74fdfbe4a7ddb
skatipelli <skatipelli@CPC-skatipel-9W.helenoftroy.com>
2022-08-10 10:16:31 +0530

    OXO Update Test Case names.

---
+++ src/test/java/TestExecute/OXO/unitTestcase/Test_DGLD_OXO_UT_Chatbot_016_NormalPath_User_Selects_to_View_the_Chatbot.java
@@ -0,0 +1,50 @@
+package TestExecute.OXO.unitTestcase;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import TestComponent.OXO.OxoHelper;
+import TestLib.Common;
+import TestLib.Login;
+
+public class Test_DGLD_OXO_UT_Chatbot_016_NormalPath_User_Selects_to_View_the_Chatbot {
+
+	String datafile = "Oxo//OxoTestData.xlsx";	
+	OxoHelper Oxo=new OxoHelper(datafile);
+	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
+	public void Validate_toggle_between_chat_options() throws Exception {
+
+		try {
+	      Oxo.click_ChatBot(); 
+		Oxo.verifychatbot("Chatbot");
+			
+		}
+		catch (Exception e) {
+			
+			Assert.fail(e.getMessage(), e);
+		} 
+	}
+	
+	
+	
+	@AfterTest
+	public void clearBrowser()
+	{
+		Common.closeAll();
+
+	}
+	
+	
+	@BeforeTest
+	  public void startTest() throws Exception {
+		System.setProperty("configFile", "Oxo\\config.properties");
+		  Login.signIn();
+		 
+		  
+	  }
+
+
+
+}
