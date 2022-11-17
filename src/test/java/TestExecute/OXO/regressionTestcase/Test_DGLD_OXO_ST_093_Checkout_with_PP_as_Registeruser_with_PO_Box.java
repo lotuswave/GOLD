173ac375195040bb68c2d34be08ce92c3eeabc80
nmanuka <nmanuka@CPC-nmanuka5-HE.helenoftroy.com>
2022-11-17 06:47:04 +0530

    new PO box address testcases, testdata

---
+++ src/test/java/TestExecute/OXO/regressionTestcase/Test_DGLD_OXO_ST_093_Checkout_with_PP_as_Registeruser_with_PO_Box.java
@@ -0,0 +1,50 @@
+package TestExecute.OXO.regressionTestcase;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import TestComponent.OXO.GoldOxoHelper;
+import TestLib.Common;
+import TestLib.Login;
+
+public class Test_DGLD_OXO_ST_093_Checkout_with_PP_as_Registeruser_with_PO_Box {
+
+	String datafile = "OXO//GoldOxoTestData.xlsx";	
+	GoldOxoHelper Oxo = new GoldOxoHelper(datafile,"DataSet");
+	
+	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
+	public void Validating_Register_user_Checkout_PO_Box_with_Paypal() throws Exception {
+
+		try {
+			Oxo.verifingHomePage();
+			Oxo.click_singinButton();
+			Oxo.Usersignin("AccountDetails");
+			Oxo.coffee_headerlinks("Coffee & Beverage");
+			Oxo.addtocart("addproduct");
+			Oxo.minicart_Checkout();
+			Oxo.addDeliveryAddress_registerUser("PO box Address");
+			Oxo.payPal_Payment("PaypalDetails");
+			
+			
+		} catch (Exception e) {
+
+			Assert.fail(e.getMessage(), e);
+		}
+	}
+
+	@AfterTest
+	public void clearBrowser() {
+		Common.closeAll();
+
+	}
+
+	@BeforeTest
+	  public void startTest() throws Exception {
+		 System.setProperty("configFile", "oxo\\config.properties");
+		  Login.signIn();
+		  Oxo.acceptPrivacy();
+	}
+
+}
