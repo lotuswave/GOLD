d0c3875a69e3baf960975592c7f4248ba31d1cc3
avayugundla <avayugundla@CPC-avayugun-FW.helenoftroy.com>
2022-11-17 15:46:33 +0530

    Account page validation for hydroflask

--- src/main/java/TestComponent/Hydroflask/GoldHydroHelper.java
+++ src/main/java/TestComponent/Hydroflask/GoldHydroHelper.java
@@ -7236,8 +7236,38 @@ catch(Exception | Error e)
 		}
 		
 	}
-	
-	
+	public void Account_page_Validation(String Dataset) throws Exception {
+		// TODO Auto-generated method stub
+		Sync.waitElementPresent("xpath", "//div[@class='m-account-nav__content']");
+		Common.clickElement("xpath", "//div[@class='m-account-nav__content']");
+		Sync.waitElementPresent("xpath", "//a[text()='My Account']");
+		Common.clickElement("xpath", "//a[text()='My Account']");
+		Sync.waitPageLoad();
+		Thread.sleep(4000);
+		
+			String Accountlinks=data.get(Dataset).get("Account Links");
+			String[] Account=Accountlinks.split(",");
+			int i=0;
+			try
+	    {
+				for(i=0;i<Account.length;i++){
+					Sync.waitElementPresent("xpath", "//div[@class='content account-nav-content']//a[text()='"+Account[i]+"']");
+					Common.clickElement("xpath", "//div[@class='content account-nav-content']//a[text()='"+Account[i]+"']");
+					Sync.waitPageLoad();
+					Thread.sleep(4000);
+					String title=Common.findElement("xpath", "//h1[@class='page-title-wrapper h2']").getText();
+					Common.assertionCheckwithReport(title.contains(Account[i]), "verifying Account page links "+Account[i],"user should navigate to the "+Account[i]+" page", "user successfully Navigated to the "+Account[i],"Failed click on the "+Account[i]);
+			
+		}
+	    }
+		catch(Exception | Error e)
+		{
+			e.printStackTrace();
+			ExtenantReportUtils.addFailedLog("validating the account page links " +Account[i],"user should Navigate to the "+Account[i]+" page","User unable to navigate to the "+Account[i],Common.getscreenShotPathforReport("user Failed to Navigate to the respective page"));
+		    Assert.fail();
+		}
+		
+	}
 	}
 		
 
