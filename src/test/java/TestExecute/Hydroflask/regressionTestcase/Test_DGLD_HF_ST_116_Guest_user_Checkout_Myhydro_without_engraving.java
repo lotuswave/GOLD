90c67a517a06997a344a015a872591ec33ab6153
avayugundla <avayugundla@CPC-avayugun-FW.helenoftroy.com>
2022-12-02 20:05:18 +0530

    my hydro testcase hydroflask

---
+++ src/test/java/TestExecute/Hydroflask/regressionTestcase/Test_DGLD_HF_ST_116_Guest_user_Checkout_Myhydro_without_engraving.java
@@ -0,0 +1,52 @@
+package TestExecute.Hydroflask.regressionTestcase;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import TestComponent.Hydroflask.GoldHydroHelper;
+import TestLib.Common;
+import TestLib.Login;
+
+public class Test_DGLD_HF_ST_116_Guest_user_Checkout_Myhydro_without_engraving {
+
+	String datafile = "Hydroflask//GoldHydroTestData.xlsx";
+	GoldHydroHelper Hydro = new GoldHydroHelper(datafile,"Engraving");
+
+	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
+	public void Validate_Guest_user_Checkout_Myhydro_without_engraving () throws Exception {
+
+		try {
+			Hydro.verifingHomePage();
+			Hydro.search_product("Myhydro Product");   
+			Hydro.Myhydro("Myhydro Product");
+			Hydro.minicart_Checkout();
+			Hydro.addDeliveryAddress_Guestuser("AccountDetails");
+            Hydro.selectshippingaddress("GroundShipping method");
+            Hydro.clickSubmitbutton_Shippingpage();
+			Hydro.updatePaymentAndSubmitOrder("PaymentDetails");
+
+		} catch (Exception e) {
+
+			Assert.fail(e.getMessage(), e);
+		}
+	}
+
+
+	@AfterTest
+	public void clearBrowser() {
+		Common.closeAll();
+
+	}
+
+	@BeforeTest
+	public void startTest() throws Exception {
+		System.setProperty("configFile", "Hydroflask\\config.properties");
+        Login.signIn();
+        Hydro.close_add();
+        Hydro.acceptPrivacy();
+
+	}
+
+}
