031b34a00248878e4686f284d7f1d13fc9183983
avayugundla <avayugundla@CPC-avayugun-FW.helenoftroy.com>
2023-04-18 13:21:54 +0530

    Create account for Ospery emea

---
+++ src/main/java/TestComponent/Osprey_EMEA/OspreyRegressionEMEA.java
@@ -0,0 +1,133 @@
+package TestComponent.Osprey_EMEA;
+
+import static org.testng.Assert.fail;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.ElementClickInterceptedException;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.Color;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.AssertJUnit;
+
+import TestLib.Automation_properties;
+import TestLib.Common;
+import TestLib.Common.SelectBy;
+import TestLib.Sync;
+import Utilities.ExcelReader;
+import Utilities.ExtenantReportUtils;
+import Utilities.Utils;
+import groovyjarjarantlr.CommonAST;
+import groovyjarjarantlr.CommonASTWithHiddenTokens;
+import groovyjarjarantlr4.v4.parse.ANTLRParser.action_return;
+
+public class OspreyRegressionEMEA {
+
+	String datafile;
+	ExcelReader excelData;
+	Map<String, Map<String, String>> data = new HashMap<>();
+	static ExtenantReportUtils report;
+	static Automation_properties automation_properties = Automation_properties.getInstance();
+
+	public OspreyRegressionEMEA(String datafile, String sheetname) {
+
+		excelData = new ExcelReader(datafile, sheetname);
+		data = excelData.getExcelValue();
+		this.data = data;
+		if (Utilities.TestListener.report == null) {
+			report = new ExtenantReportUtils("Osprey_EMEA");
+			report.createTestcase("Osprey_EMEATestCases");
+		} else {
+			this.report = Utilities.TestListener.report;
+		}
+
+	}
+
+	public int getpageresponce(String url) throws MalformedURLException, IOException {
+		HttpURLConnection c = (HttpURLConnection) new URL(url).openConnection();
+		c.setRequestMethod("HEAD");
+		c.connect();
+		int r = c.getResponseCode();
+
+		return r;
+	}
+
+	public void verifingHomePage() {
+		// TODO Auto-generated method stub
+		try
+		{
+			Sync.waitPageLoad();
+			int size = Common.findElements("xpath", "//a[@aria-label='Home page link']").size();
+			Common.assertionCheckwithReport(
+					size > 0 && Common.getPageTitle().contains("Home page"),
+					"validating store logo on the homwpage", "System directs the user to the Homepage and store logo should display",
+					"Sucessfully user navigates to the home page and logo has been displayed", "Failed to navigate to the homepage and logo is not displayed");	
+		}
+		catch(Exception | Error e)
+		{
+			e.printStackTrace();
+			ExtenantReportUtils.addFailedLog("validating store logo on the homwpage", "System directs the user to the Homepage and store logo should display",
+					"Unable to navigate to the homepage and logo is not displayed", "Failed to navigate to the homepage and logo is not displayed");
+			
+			Assert.fail();
+		}
+		
+	}
+
+	public void Create_Account(String Dataset) {
+		// TODO Auto-generated method stub
+		try
+		{
+			Sync.waitElementPresent(30, "xpath", "//button[@aria-controls='desktop-account-nav']");
+			Common.clickElement("xpath", "//button[@aria-controls='desktop-account-nav']");
+			Common.clickElement("xpath", "//li[@class='nav item']//a[text()='Create an Account']");
+			Sync.waitImplicit(30);
+			Common.assertionCheckwithReport(Common.getPageTitle().contains("Create New Customer Account"),
+					"validating navigation to the create new account page",
+					"User should navigate to the create account page",
+					"Sucessfully user navigates to the Create account page",
+					"Failed to navigate to the Create account page");
+			Common.textBoxInput("xpath", "//input[@name='firstname']", data.get(Dataset).get("FirstName"));
+			Common.textBoxInput("xpath", "//input[@name='lastname']", data.get(Dataset).get("LastName"));
+			Common.textBoxInput("xpath", "//input[@name='email']", Utils.getEmailid());
+			Common.textBoxInput("xpath", "//input[@name='password']",data.get(Dataset).get("Password"));
+			Common.textBoxInput("xpath", "//input[@name='password_confirmation']",data.get(Dataset).get("Confirm Password"));
+			Thread.sleep(4000);
+			Common.clickElement("xpath", "//span[text()='Sign Up']");
+			Sync.waitImplicit(30);
+			Thread.sleep(4000);
+			String message=Common.findElement("xpath", "//div[@class='a-message__container-inner']").getText();
+			Common.assertionCheckwithReport(message.contains("Thank you for registering with Osprey Store.")&&Common.getPageTitle().contains("My Account"),
+					"validating navigation to the account page after clicking on sign up button",
+					"User should navigate to the My account page after clicking on the Signup",
+					"Sucessfully user navigates to the My account page after clickng on thr signup button",
+					"Failed to navigate to the My account page after clicking on the signup button");
+		
+		}
+		catch(Exception | Error e)
+		{
+			e.printStackTrace();
+			ExtenantReportUtils.addFailedLog("validating navigation to the account page after clicking on sign up button",
+					"User should navigate to the My account page after clicking on the Signup",
+					"Unable to navigate to the My account page after clicking on the signup button",
+					"Failed to navigate to the My account page after clicking on the signup button");
+			Assert.fail();
+		}
+		
+	}
+}
---
+++ src/test/java/TestExecute/Osprey_EMEA/regressionTestcase/Test_DGLD_OS_EU_001_Create_Acoount_Funtionality.java
@@ -0,0 +1,45 @@
+package TestExecute.Osprey_EMEA.regressionTestcase;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import TestComponent.Osprey_EMEA.OspreyRegressionEMEA;
+import TestLib.Common;
+import TestLib.Login;
+
+public class Test_DGLD_OS_EU_001_Create_Acoount_Funtionality {
+
+	String datafile = "Osprey_EMEA//GoldOspreyemea.xlsx";
+	OspreyRegressionEMEA Osprey_ReEu = new OspreyRegressionEMEA(datafile,"CreateAccount");
+
+	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
+	public void Verifying_the_Create_account_Funtionality () throws Exception {
+
+		try {
+        Osprey_ReEu.verifingHomePage();
+        Osprey_ReEu.Create_Account("Create Account");
+        
+		} catch (Exception e) {
+
+			Assert.fail(e.getMessage(), e);
+		}
+	}
+
+
+	@AfterTest
+	public void clearBrowser() {
+		Common.closeAll();
+
+	}
+
+	@BeforeTest
+	public void startTest() throws Exception {
+		System.setProperty("configFile", "Osprey_EMEA\\config.properties");
+        Login.signIn();
+        
+
+	}
+
+}
