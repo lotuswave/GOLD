b4578c770a72e65f95d767edb4d1c34df0b0ecc6
abogi <abogi@CPC-abogi-THET4.helenoftroy.com>
2023-06-19 14:57:05 +0530

    Mini cart validation for osprey

--- src/main/java/TestComponent/Osprey_EMEA/OspreyRegressionEMEA.java
+++ src/main/java/TestComponent/Osprey_EMEA/OspreyRegressionEMEA.java
@@ -969,6 +969,7 @@ public void search_product(String Dataset) {
 public void addtocart(String Dataset) {
 	// TODO Auto-generated method stub
 	String products = data.get(Dataset).get("Products");
+	String Productsize=data.get(Dataset).get("Size");
 	System.out.println(products);
 	try {
 		Sync.waitPageLoad();
@@ -989,6 +990,8 @@ public void addtocart(String Dataset) {
 		Thread.sleep(6000);
 		Sync.waitElementPresent(30, "xpath", "//img[@alt='" + products + "']");
 		Common.clickElement("xpath", "//img[@alt='" + products + "']");
+		Sync.waitElementPresent("xpath", "//div[@data-option-label='"+ Productsize +"']");
+		Common.clickElement("xpath", "//div[@data-option-label='" +Productsize+"']");
 		Sync.waitPageLoad(30);
 		Thread.sleep(6000);
 		Sync.waitElementVisible(30, "xpath", "//div[@class='m-product-overview__info-top']//h1");
@@ -3296,6 +3299,202 @@ public void My_Orders_Page(String Dataset) {
 				Common.getscreenShot("Failed to Display My order Number in the My orders page"));
 		Assert.fail();
 	}
+}
+public void clickontheproduct_and_image(String Dataset) {
+	// TODO Auto-generated method stub
+	String product = data.get(Dataset).get("Products");
+	System.out.println(product);
+	String color = data.get(Dataset).get("Colorproduct");
+	System.out.println(color);
+	try {
+		String minicartproduct = Common
+				.findElement("xpath", "//a[@class='a-product-name' and @title='" + product + "']").getText();
+		Common.clickElement("xpath", "//a[@class='a-product-name' and @title='" + product + "']");
+		Sync.waitPageLoad();
+		Thread.sleep(3000);
+		System.out.println(minicartproduct);
+		Common.assertionCheckwithReport(product.contains(minicartproduct),
+				"validating the product navigating to the PDP page",
+				"The product Should be navigates to the PDP page", "Successfully product navigates to the PDP page",
+				"Failed to Navigates Product to the PDP page");
+		click_minicart();
+		String minicartimage = Common.findElement("xpath", "//img[contains(@alt,'" + color + "')]")
+				.getAttribute("alt");
+		Common.clickElement("xpath", "//img[contains(@alt,'" + color + "')]");
+		Sync.waitPageLoad();
+		Thread.sleep(3000);
+		Common.assertionCheckwithReport(minicartimage.contains(color),
+				"validating the product navigating to the PDP page",
+				"The product Should be navigates to the PDP page", "Successfully product navigates to the PDP page",
+				"Failed to Navigates Product to the PDP page");
+	} catch (Exception | Error e) {
+		e.printStackTrace();
+		ExtenantReportUtils.addFailedLog("validating the product navigating to the PDP page",
+				"The product Should be navigates to the PDP page", " unable to Navigates Product to the PDP page",
+				Common.getscreenShot("Failed to Navigates Product to the PDP page"));
+		Assert.fail();
+	}
+
+}
+public void minicart_freeshipping() {
+	// TODO Auto-generated method stub
+	try {
+		click_minicart();
+		String Freeshipping = Common
+				.findElement("xpath", "//div[@class='m-progress-bar false']//div[contains(@class,'label-')]")
+				.getText();
+		Common.assertionCheckwithReport(Freeshipping.equals("Good news: your order will be delivered for Free."),
+				"validating the free shipping in mini cart",
+				"Free shipping should be avaliable for selected products",
+				"Successfully free shipping is appiled for selected products", "Failed to see free shipping");
+
+	} catch (Exception | Error e) {
+		e.printStackTrace();
+		ExtenantReportUtils.addFailedLog("validating the free shipping in mini cart",
+				"Free shipping should be avaliable for selected products",
+				"unable to apply free shipping for the selected products",
+				Common.getscreenShot("Failed to see free shipping bar"));
+		Assert.fail();
+	}
+
+}
+
+public void minicart_delete(String Dataset) {
+	// TODO Auto-generated method stub
+	String deleteproduct = data.get(Dataset).get("Colorproduct");
+	try {
+		Sync.waitElementPresent(30, "xpath", "//span[@class='c-mini-cart__subtotal-amount']//span");
+		String subtotal = Common.getText("xpath", "//span[@class='c-mini-cart__subtotal-amount']//span")
+				.replace("£", "");
+		Float subtotalvalue = Float.parseFloat(subtotal);
+		String productname = Common
+				.findElement("xpath", "(//div[@class='m-mini-product-card__info']//a[@class='a-product-name'])[1]")
+				.getText();
+		String productamount1 = Common.getText("xpath", "(//span[@class='minicart-price']//span)[1]").replace("£",
+				"");
+		Float productamount1value = Float.parseFloat(productamount1);
+		if (productname.equals(deleteproduct)) {
+			Sync.waitElementPresent(30, "xpath",
+					"(//div[@class='m-mini-product-card__info']//span[contains(@class,'icon-cart__remove')])[1]");
+			Common.clickElement("xpath",
+					"(//div[@class='m-mini-product-card__info']//span[contains(@class,'icon-cart__remove')])[1]");
+			Sync.waitElementPresent("xpath", "//button[contains(@class,'a-btn a-btn--primary action-p')]//span");
+			Common.clickElement("xpath", "//button[contains(@class,'a-btn a-btn--primary action-p')]//span");
+		} else {
+			Assert.fail();
+		}
+		Thread.sleep(6000);
+		String subtotal1 = Common.getText("xpath", "//span[@class='c-mini-cart__subtotal-amount']//span")
+				.replace("£", "");
+		Float subtotal1value = Float.parseFloat(subtotal1);
+		Thread.sleep(8000);
+		String productamount = Common.getText("xpath", "//span[@class='minicart-price']//span").replace("£", "");
+		Float productamountvalue = Float.parseFloat(productamount);
+		Float Total = subtotalvalue - productamount1value;
+		String ExpectedTotalAmmount2 = new BigDecimal(Total).setScale(2, BigDecimal.ROUND_HALF_UP).toString();
+		Thread.sleep(4000);
+		Common.assertionCheckwithReport(ExpectedTotalAmmount2.equals(subtotal1),
+				"validating the delete operation and subtotal",
+				"The product should be delete from mini cart and subtotal should change",
+				"Successfully product delete from the mini cart and subtotal has been changed",
+				"Failed to delete the product from cart and subtotal not changed");
+
+	} catch (Exception | Error e) {
+		e.printStackTrace();
+		ExtenantReportUtils.addFailedLog("validating the delete operation and subtotal",
+				"The product should be delete from mini cart and subtotal should change",
+				"unable to delete product from the mini cart and subtotal has not changed", Common.getscreenShot(
+						"Failed to delete the product from the mini cart and subtotal has not changed"));
+		Assert.fail();
+	}
+}
+
+public void minicart_product_close() {
+	// TODO Auto-generated method stub
+	try {
+
+		Common.clickElement("xpath", "//span[contains(@class,'icon-cart__r')]");
+		Sync.waitElementPresent("xpath", "//div[@class='modal-popup confirm _show']");
+		String minicartpopup = Common.findElement("xpath", "//div[@class='modal-popup confirm _show']")
+				.getAttribute("class");
+		Sync.waitElementInvisible(30, "xpath", "//div[@data-role='spinner' and @style='display: none;']");
+		Common.assertionCheckwithReport(minicartpopup.contains("_show"),
+				"validating the popup when you click on delete", "The Popup should be displayed",
+				"Successfully popup is displayed when we click on the delete button",
+				"Failed to Display the popup");
+		String popup = Common.findElement("xpath", "//h2[contains(text(),'Remove')]").getText();
+		if (popup.equals("Remove Item")) {
+			Common.clickElement("xpath", "//button[contains(@class,'a-btn a-btn--secondary acti')]");
+		} else {
+			Assert.fail();
+		}
+		Common.clickElement("xpath", "//span[contains(@class,'icon-cart__r')]");
+		Sync.waitElementPresent("xpath", "//div[@class='modal-popup confirm _show']");
+		Sync.waitElementInvisible(30, "xpath", "//div[@data-role='spinner' and @style='display: none;']");
+		Common.assertionCheckwithReport(minicartpopup.contains("_show"),
+				"validating the popup when you click on delete", "The Popup should be displayed",
+				"Successfully popup is displayed when we click on the delete button",
+				"Failed to Display the popup");
+		if (popup.equals("Remove Item")) {
+
+			Common.clickElement("xpath", "//button[@data-role='closeBtn' and @aria-label='Close']");
+		} else {
+			Assert.fail();
+		}
+	} catch (Exception | Error e) {
+		e.printStackTrace();
+		ExtenantReportUtils.addFailedLog("validating the close and cancel functionality",
+				"User should able to click on close and cancel button",
+				"unable to click on close and cancel button",
+				Common.getscreenShot("Failed to Click on close and cancel button"));
+
+		Assert.fail();
+	}
+
+}
+
+public void minicart_validation(String Dataset) {
+	// TODO Auto-generated method stub
+	String UpdataedQuntityinminicart = data.get(Dataset).get("Quantity");
+	try {
+
+		String Subtotal = Common.getText("xpath", "//span[@class='c-mini-cart__subtotal-amount']//span")
+				.replace("£", "");
+		Float subtotalvalue = Float.parseFloat(Subtotal);
+		Sync.waitElementPresent("xpath", "//select[@class='a-select-menu cart-item-qty']");
+		Common.clickElement("xpath", "//select[@class='a-select-menu cart-item-qty']");
+		Common.dropdown("xpath", "//select[@class='a-select-menu cart-item-qty']", Common.SelectBy.VALUE,
+				UpdataedQuntityinminicart);
+		Common.clickElement("xpath", "//span[text()='Update']");
+		Thread.sleep(4000);
+		Sync.waitElementPresent("xpath", "//p[@class='c-mini-cart__total-counter']//strong");
+		String cart = Common.findElement("xpath", "//p[@class='c-mini-cart__total-counter']//strong").getText();
+		System.out.println(cart);
+		String Subtotal2 = Common.getText("xpath", "//span[@class='c-mini-cart__subtotal-amount']//span")
+				.replace("£", "");
+		Float subtotalvalue2 = Float.parseFloat(Subtotal2);
+		Float Total = subtotalvalue * 2;
+		String ExpectedTotalAmmount2 = new BigDecimal(Total).setScale(2, BigDecimal.ROUND_HALF_UP).toString();
+		System.out.println(UpdataedQuntityinminicart);
+		System.out.println(cart);
+		System.out.println(ExpectedTotalAmmount2);
+		System.out.println(Subtotal2);
+		Common.assertionCheckwithReport(
+				UpdataedQuntityinminicart.equals(cart) && ExpectedTotalAmmount2.equals(Subtotal2),
+				"validating the product update quantity and subtotal",
+				"The product Quantity should be update in mini cart and subtotal should change",
+				"Successfully product quantity updated and subtotal has been changed",
+				"Failed to update the product quantity from cart and subtotal not changed");
+
+	} catch (Exception | Error e) {
+		e.printStackTrace();
+		ExtenantReportUtils.addFailedLog("validating the product update quantity and subtotal",
+				"The product Quantity should be update in mini cart and subtotal should change",
+				"unable to update the product quantity and subtotal has not be changed",
+				Common.getscreenShot("Failed to update the product quantity from cart and subtotal not changed"));
+		Assert.fail();
+	}
+
 }
 
 }
