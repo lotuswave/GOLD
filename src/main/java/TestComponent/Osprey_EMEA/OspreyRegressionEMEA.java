16c7a2812ec1c18bf5ae815415d95fe913ab21de
avayugundla <avayugundla@CPC-avayu-3AUJR.helenoftroy.com>
2023-09-11 21:20:48 +0530

    PP multiple products for osprey

--- src/main/java/TestComponent/Osprey_EMEA/OspreyRegressionEMEA.java
+++ src/main/java/TestComponent/Osprey_EMEA/OspreyRegressionEMEA.java
@@ -5400,8 +5400,8 @@ public class OspreyRegressionEMEA {
 			Common.assertionCheckwithReport(sizes > 0, "Validating the payment section page",
 					"payment section should be displayed", "sucessfully payment section has been displayed",
 					"Failed to displayed the payment section");
-			Sync.waitElementPresent(30, "xpath", "//label[contains(@for,'billing-address')]");
-			Common.clickElement("xpath", "//label[contains(@for,'billing-address')]");
+			Sync.waitElementPresent(30, "xpath", "//label[contains(@for,'billing-address')]//span");
+			Common.clickElement("xpath", "//label[contains(@for,'billing-address')]//span");
 			Common.textBoxInput("xpath", "//input[@name='firstname']", data.get(dataSet).get("FirstName"));
 			Common.textBoxInput("xpath", "//input[@name='lastname']", data.get(dataSet).get("LastName"));
 			Common.textBoxInput("xpath", "//input[@name='street[0]']", data.get(dataSet).get("Street"));
@@ -8910,5 +8910,80 @@ catch(Exception | Error e)
 		
 	}
 
+	public String Reg_BillingAddress(String dataSet) {
+		// TODO Auto-generated method stub
+		String update = "";
+		try {
+			Sync.waitPageLoad();
+			Thread.sleep(4000);
+			Sync.waitElementPresent("xpath", "//label[@for='stripe_payments']");
+			Common.clickElement("xpath", "//label[@for='stripe_payments']");
+			int sizes = Common.findElements("xpath", "//label[@for='stripe_payments']").size();
+			Common.clickElement("xpath", "//label[@for='stripe_payments']");
+			Common.assertionCheckwithReport(sizes > 0, "Validating the payment section page",
+					"payment section should be displayed", "sucessfully payment section has been displayed",
+					"Failed to displayed the payment section");
+			Thread.sleep(6000);
+			Sync.waitElementPresent(30, "xpath", "//label[contains(@for,'billing-address')]//span");
+			Common.clickElement("xpath", "//label[contains(@for,'billing-address')]//span");
+			Thread.sleep(5000);
+			Common.dropdown("xpath", "//select[@aria-label='Billing Address']", SelectBy.TEXT, "New Address");
+			Common.textBoxInput("xpath", "//input[@name='firstname']", data.get(dataSet).get("FirstName"));
+			Common.textBoxInput("xpath", "//input[@name='lastname']", data.get(dataSet).get("LastName"));
+			Common.textBoxInput("xpath", "//input[@name='street[0]']", data.get(dataSet).get("Street"));
+			Thread.sleep(4000);
+			String text = Common.findElement("xpath", "//input[@name='street[0]']").getAttribute("value");
+			Sync.waitPageLoad();
+			Thread.sleep(5000);
+			Common.textBoxInput("xpath", "//input[@name='city']", data.get(dataSet).get("City"));
+			System.out.println(data.get(dataSet).get("City"));
+
+//			Common.actionsKeyPress(Keys.PAGE_DOWN);
+			Thread.sleep(3000);
+			 if(Common.getCurrentURL().contains("stage3"))
+             {
+				  Thread.sleep(4000);
+                 Common.scrollIntoView("xpath", "//select[@name='region_id']");
+                 Common.dropdown("xpath", "//select[@name='region_id']",Common.SelectBy.TEXT, data.get(dataSet).get("Region"));
+                 Thread.sleep(3000);
+                 String Shippingvalue = Common.findElement("xpath", "//select[@name='region_id']")
+                         .getAttribute("value");
+                 System.out.println(Shippingvalue);
+             }
+			 else
+			 {
+				 Common.scrollIntoView("xpath", "//input[@placeholder='State/Province']");
+				Common.textBoxInput("xpath", "//input[@placeholder='State/Province']", data.get(dataSet).get("Region"));
+			}
+			Thread.sleep(2000);
+			// Common.textBoxInputClear("xpath", "//input[@name='postcode']");
+			Common.textBoxInput("xpath", "//div[contains(@name,'payments.postcode')]//input[@name='postcode']",
+					data.get(dataSet).get("postcode"));
+			Thread.sleep(5000);
+
+			Common.textBoxInput("xpath", "//div[@class='field _required']//input[@name='telephone']",
+					data.get(dataSet).get("phone"));
+			Common.clickElement("xpath", "//span[text()='Update']");
+			Sync.waitPageLoad();
+			Thread.sleep(5000);
+			update = Common.findElement("xpath", "(//span[@data-bind='text: currentBillingAddress().region'])[2]").getText();
+			System.out.println("update"+update);
+			Common.assertionCheckwithReport(
+					update.contains("Connecticut") || text.contains("Wallingford") || text.contains("Aberdeen"),
+					"verifying the Billing address form in payment page",
+					"Billing address should be saved in the payment page",
+					"Sucessfully Billing address form should be Display ",
+					"Failed to display the Billing address in payment page");
+
+		} catch (Exception | Error e) {
+			e.printStackTrace();
+			ExtenantReportUtils.addFailedLog("verifying the Billing address form in payment page",
+					"Billing address should be saved in the payment page",
+					"Unable to display the Billing address in payment page",
+					Common.getscreenShotPathforReport("Failed to display the Billing address in payment page"));
+			Assert.fail();
+		}
+		return update;
 	}
+}
 
---
+++ src/test/java/TestExecute/Osprey_EMEA/regressionTestcase/TEST_DGLD_OS_COMMON_106_Placeorder_Register_user_checkout_PP_with_Multiple_products_with_Discount_differentbilling_and_shipping.java
@@ -0,0 +1,56 @@
+package TestExecute.Osprey_EMEA.regressionTestcase;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import TestComponent.Osprey_EMEA.OspreyRegressionEMEA;
+import TestLib.Common;
+import TestLib.Login;
+
+public class TEST_DGLD_OS_COMMON_106_Placeorder_Register_user_checkout_PP_with_Multiple_products_with_Discount_differentbilling_and_shipping {
+
+	String datafile = "Osprey_EMEA//GoldOspreyemea.xlsx";
+	OspreyRegressionEMEA Osprey_ReEu = new OspreyRegressionEMEA(datafile,"Checkout payments");
+
+	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
+	public void Verifying_Placeorder_Register_user_checkout_PP_with_Multiple_products_with_Discount_differentbilling_and_shipping () throws Exception {
+
+		try {
+        Osprey_ReEu.verifingHomePage();
+        Osprey_ReEu.click_singinButton();
+        Osprey_ReEu.Login_Account("Account");
+        Osprey_ReEu.search_product("Product");
+        Osprey_ReEu.addtocart("Product");
+        Osprey_ReEu.Bagpacks_headerlinks("Backpacks & Bags");
+        Osprey_ReEu.simple_addtocart("Simple product");  
+        Osprey_ReEu.minicart_Checkout();
+        Osprey_ReEu.RegaddDeliveryAddress("Account");
+        Osprey_ReEu.selectshippingmethod("GroundShipping method");
+        Osprey_ReEu.clickSubmitbutton_Shippingpage();
+        Osprey_ReEu.Reg_BillingAddress("BillingDetails");
+        Osprey_ReEu.payPal_Payment("PaypalDetails");
+        
+		} catch (Exception e) {
+
+			Assert.fail(e.getMessage(), e);
+		}
+	}
+
+
+	@AfterTest
+	public void clearBrowser() {
+		Common.closeAll();
+
+	}
+
+	@BeforeTest
+	public void startTest() throws Exception {
+		System.setProperty("configFile", "Osprey_EMEA\\config.properties");
+        Login.signIn();
+        
+
+	}
+
+}
