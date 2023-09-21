8ae105bc0e4380715796719265e5cb877d04f14a
avayugundla <avayugundla@CPC-avayu-3AUJR.helenoftroy.com>
2023-09-21 12:24:35 +0530

    33 testcase changes for osprey emea

---
+++ src/test/java/TestExecute/Osprey_EMEA/regressionTestcase/TEST_DGLD_OS_COMMON_033_Guest_user_checkout_PP_with_Multiple_products_with_Discount_samebilling_and_shipping.java
@@ -0,0 +1,53 @@
+package TestExecute.Osprey_EMEA.regressionTestcase;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import TestComponent.Osprey_EMEA.OspreyRegressionEMEA;
+import TestLib.Common;
+import TestLib.Login;
+
+public class TEST_DGLD_OS_COMMON_033_Guest_user_checkout_PP_with_Multiple_products_with_Discount_samebilling_and_shipping {
+
+	String datafile = "Osprey_EU//GoldOspreyemea.xlsx";
+	OspreyRegressionEMEA Osprey_ReEu = new OspreyRegressionEMEA(datafile,"Checkout payments");
+
+	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
+	public void Guest_user_checkout_PP_with_Multiple_products_with_Discount_samebilling_and_shipping () throws Exception {
+
+		try {
+        Osprey_ReEu.verifingHomePage();
+        Osprey_ReEu.search_product("Product");
+        Osprey_ReEu.addtocart("Product");
+        Osprey_ReEu.Bagpacks_headerlinks("Backpacks & Bags");
+        Osprey_ReEu.simple_addtocart("Simple product");  
+        Osprey_ReEu.minicart_Checkout();
+        Osprey_ReEu.RegaddDeliveryAddress("Account");
+        Osprey_ReEu.addDeliveryAddress_Guestuser("GroundShipping method");
+        Osprey_ReEu.clickSubmitbutton_Shippingpage();
+        Osprey_ReEu.payPal_Payment("PaypalDetails");
+        
+		} catch (Exception e) {
+
+			Assert.fail(e.getMessage(), e);
+		}
+	}
+
+
+	@AfterTest
+	public void clearBrowser() {
+		Common.closeAll();
+
+	}
+
+	@BeforeTest
+	public void startTest() throws Exception {
+		System.setProperty("configFile", "Osprey_EMEA\\config.properties");
+        Login.signIn();
+        
+
+	}
+
+}
