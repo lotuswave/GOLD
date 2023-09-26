ac05f8a9444ca3b6c7e24531b432f93e1bb357ac
skasarla <skasarla@CPC-skasa-DD8QS.helenoftroy.com>
2023-09-26 14:25:44 +0530

    Gifttcard PLP Changes

---
+++ src/test/java/TestExecute/Osprey_EMEA/regressionTestcase/TEST_DGLD_OS_COMMON_096_RegisterUser_validate_Giftcard_PLP_page.java
@@ -0,0 +1,59 @@
+package TestExecute.Osprey_EMEA.regressionTestcase;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import TestComponent.Osprey_EMEA.OspreyRegressionEMEA;
+import TestLib.Common;
+import TestLib.Login;
+
+public class TEST_DGLD_OS_COMMON_096_RegisterUser_validate_Giftcard_PLP_page {
+
+	String datafile = "Osprey_EMEA//GoldOspreyemea.xlsx";
+	OspreyRegressionEMEA Osprey_ReEu = new OspreyRegressionEMEA(datafile,"Giftcard Payments");
+
+	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
+	public void Verifying_Register_user_validate_Gift_card_PLP_page () throws Exception {
+
+		try {
+        Osprey_ReEu.verifingHomePage();
+        Osprey_ReEu.click_singinButton();
+        Osprey_ReEu.Login_Account("Account");
+        Osprey_ReEu.validate_GIFT_CARD_PLP();
+        /*Osprey_ReEu.Gift_cards("X-mas Gift Card");
+        Osprey_ReEu.Card_Value("price");
+        Osprey_ReEu.minicart_Checkout();
+        Osprey_ReEu.RegaddDeliveryAddress("Account");
+        Osprey_ReEu.selectshippingmethod("GroundShipping method");
+        Osprey_ReEu.clickSubmitbutton_Shippingpage();
+        Osprey_ReEu.clickSubmitbutton_Shippingpage();
+        Osprey_ReEu.updatePaymentAndSubmitOrder("CCVisacard");*/
+        
+       
+        
+       
+        
+		} catch (Exception e) {
+
+			Assert.fail(e.getMessage(), e);
+		}
+	}
+
+
+	@AfterTest
+	public void clearBrowser() {
+		Common.closeAll();
+
+	}
+
+	@BeforeTest
+	public void startTest() throws Exception {
+		System.setProperty("configFile", "Osprey_EMEA\\config.properties");
+        Login.signIn();
+        
+
+	}
+
+}
