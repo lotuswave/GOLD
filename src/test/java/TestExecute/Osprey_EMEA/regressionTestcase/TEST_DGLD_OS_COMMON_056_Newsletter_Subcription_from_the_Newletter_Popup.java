15d6208128cac054c87c37f78d9494cc3fccc3d5
avayugundla <avayugundla@CPC-avayu-3AUJR.helenoftroy.com>
2023-09-29 17:10:51 +0530

    Testcase name changes for osprey EMEA

--- src/test/java/TestExecute/Osprey_EMEA/regressionTestcase/TEST_DGLD_OS_COMMON_056_Newsletter_Subcription_from_the_Newletter_Popup.java
+++ src/test/java/TestExecute/Osprey_EMEA/regressionTestcase/TEST_DGLD_OS_COMMON_056_Newsletter_Subcription_from_the_Newletter_Popup.java
@@ -9,7 +9,7 @@ import TestComponent.Osprey_EMEA.OspreyRegressionEMEA;
 import TestLib.Common;
 import TestLib.Login;
 
-public class TEST_DGLD_OS_CT_ST_COMMON_126_Newsletter_Subcription_from_the_Newletter_Popup {
+public class TEST_DGLD_OS_COMMON_056_Newsletter_Subcription_from_the_Newletter_Popup {
 
 	String datafile = "Osprey_EMEA//GoldOspreyemea.xlsx";
 	OspreyRegressionEMEA Osprey_ReEu = new OspreyRegressionEMEA(datafile,"Checkout payments");
