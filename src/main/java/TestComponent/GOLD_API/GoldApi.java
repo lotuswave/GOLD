8eb3c4dd1af31d6976db1c776870657dbc7e935e
avayugundla <avayugundla@CPC-avayu-3AUJR.helenoftroy.com>
2023-11-22 14:48:12 +0530

    Api code patchs

---
+++ src/main/java/TestComponent/GOLD_API/GoldApi.java
@@ -0,0 +1,182 @@
+package TestComponent.GOLD_API;
+   
+import static org.testng.Assert.fail;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.ElementClickInterceptedException;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.Color;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.AssertJUnit;
+
+import TestLib.Automation_properties;
+import TestLib.Common;
+import TestLib.Common.SelectBy;
+import TestLib.Sync;
+import Utilities.ExcelReader;
+import Utilities.ExtenantReportUtils;
+import Utilities.Utils;
+import groovyjarjarantlr.CommonAST;
+import groovyjarjarantlr.CommonASTWithHiddenTokens;
+import groovyjarjarantlr4.v4.parse.ANTLRParser.action_return;
+
+public class GoldApi {
+
+	String datafile;
+	ExcelReader excelData;
+	Map<String, Map<String, String>> data = new HashMap<>();
+	private String String;
+	static ExtenantReportUtils report;
+	static Automation_properties automation_properties = Automation_properties.getInstance();
+
+	public GoldApi(String datafile, String sheetname) {
+
+		excelData = new ExcelReader(datafile, sheetname);
+		data = excelData.getExcelValue();
+		this.data = data;
+		if (Utilities.TestListener.report == null) {
+			report = new ExtenantReportUtils("Osprey_EMEA");
+			report.createTestcase("Osprey_EMEATestCases");
+		} else {
+			this.report = Utilities.TestListener.report;
+		}
+
+	}
+	
+	public void verifingHomePage() {
+		// TODO Auto-generated method stub
+		try {
+			Sync.waitPageLoad();
+			if(Common.getCurrentURL().contains("osprey.com/gb/"))
+			{
+				Close_Geolocation();
+			     acceptPrivacy();
+				int size = Common.findElements("xpath", "//a[@class='a-logo']").size();
+				System.out.println(size);
+				System.out.println(Common.getPageTitle());
+				Common.assertionCheckwithReport(size > 0 && Common.getPageTitle().contains("Osprey"),
+						"validating store logo on the homwpage",
+						"System directs the user to the Homepage and store logo should display",
+						"Sucessfully user navigates to the home page and logo has been displayed",
+						"Failed to navigate to the homepage and logo is not displayed");
+			}
+			else if(Common.getCurrentURL().contains("stage3") || Common.getCurrentURL().contains("preprod"))
+			{
+				close_add();
+				int size = Common.findElements("xpath", "//a[@class='a-logo']").size();
+				System.out.println(size);
+				System.out.println(Common.getPageTitle());
+				Common.assertionCheckwithReport(size > 0 && Common.getPageTitle().contains("Home page"),
+						"validating store logo on the homwpage",
+						"System directs the user to the Homepage and store logo should display",
+						"Sucessfully user navigates to the home page and logo has been displayed",
+						"Failed to navigate to the homepage and logo is not displayed");
+			}
+			else if(Common.getCurrentURL().contains("preprod.osprey.com/gb/"))
+			{
+				
+				 acceptPrivacy();
+				 Close_Geolocation();
+				int size = Common.findElements("xpath", "//a[@class='a-logo']").size();
+				System.out.println(size);
+				System.out.println(Common.getPageTitle());
+				Common.assertionCheckwithReport(size > 0 && Common.getPageTitle().contains("Home page"),
+						"validating store logo on the homwpage",
+						"System directs the user to the Homepage and store logo should display",
+						"Sucessfully user navigates to the home page and logo has been displayed",
+						"Failed to navigate to the homepage and logo is not displayed");
+			}
+			else
+			{
+//			Close_Geolocation();
+//			close_add();
+		     acceptPrivacy();
+			int size = Common.findElements("xpath", "//a[@class='a-logo']").size();
+			System.out.println(size);
+			System.out.println(Common.getPageTitle());
+			Common.assertionCheckwithReport(size > 0 && Common.getPageTitle().contains("Osprey") || size > 0,
+					"validating store logo on the homwpage",
+					"System directs the user to the Homepage and store logo should display",
+					"Sucessfully user navigates to the home page and logo has been displayed",
+					"Failed to navigate to the homepage and logo is not displayed");
+			}
+		} catch (Exception | Error e) {
+			e.printStackTrace();
+			ExtenantReportUtils.addFailedLog("validating store logo on the homwpage",
+					"System directs the user to the Homepage and store logo should display",
+					"Unable to navigate to the homepage and logo is not displayed",
+					"Failed to navigate to the homepage and logo is not displayed");
+
+			Assert.fail();
+		}
+
+	}
+	
+	public void Close_Geolocation() {
+		// TODO Auto-generated method stub
+		try {
+			
+			Sync.waitElementPresent("xpath", "(//button[@data-role='closeBtn'])[2]");
+			Common.clickElement("xpath", "(//button[@data-role='closeBtn'])[2]");
+			
+		
+	}catch(Exception | Error e)
+	{
+		e.printStackTrace();
+		Assert.fail();
+	}
+	
+
+	}
+	
+	public void acceptPrivacy() {
+
+		Common.clickElement("id", "truste-consent-button");
+	}
+	
+	public void close_add() throws Exception {
+        // TODO Auto-generated method stub
+        Thread.sleep(3000);
+        int sizesframe = Common.findElements("xpath", "//div[@data-testid='POPUP']").size();
+        System.out.println(sizesframe);
+        if (sizesframe > 0) {
+            Common.actionsKeyPress(Keys.PAGE_UP);
+            Thread.sleep(4000);
+            Sync.waitElementPresent("xpath", "//button[contains(@class,'needsclick klaviyo-close-form kl-private-reset-css-Xuajs1')]");
+            Common.clickElement("xpath", "//button[contains(@class,'needsclick klaviyo-close-form kl-private-reset-css-Xuajs1')]");
+        }
+        else {
+
+            Common.switchFrames("xpath", "//div[@class='preloaded_lightbox']/iframe");
+            Sync.waitElementPresent("xpath", "//button[contains(@aria-label,'Close') and @id='button3']");
+            Common.clickElement("xpath", "//button[contains(@aria-label,'Close') and @id='button3']");
+            Common.switchToDefault();
+            }
+
+ 
+
+    }
+	
+}
---
+++ src/test/java/TestExecute/API/Test_ST_Osp_001_Guest_user_checkout_with_1_Line_Item_QTY2_with_CC.java
@@ -0,0 +1,49 @@
+package TestExecute.API;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import TestComponent.GOLD_API.GoldApi;
+import TestComponent.Holley.GoldHolleyHelper;
+import TestLib.Common;
+import TestLib.Login;
+
+public class Test_ST_Osp_001_Guest_user_checkout_with_1_Line_Item_QTY2_with_CC {
+
+	String datafile = "API//GoldApiTestData.xlsx";
+	GoldApi API = new GoldApi(datafile,"Osprey_US");
+
+	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
+	public void Validate_Guest_user_checkout_with_1_Line_Item_QTY2_with_CC () throws Exception {
+
+		try {
+			
+			
+			API.verifingHomePage();	
+			
+
+
+		} catch (Exception e) {
+
+			Assert.fail(e.getMessage(), e);
+		}
+	}
+
+
+	@AfterTest
+	public void clearBrowser() {
+		Common.closeAll();
+
+	}
+
+	@BeforeTest
+	public void startTest() throws Exception {
+		System.setProperty("configFile", "Gold_API\\config.properties");
+        Login.signIn();
+
+
+	}
+
+}
