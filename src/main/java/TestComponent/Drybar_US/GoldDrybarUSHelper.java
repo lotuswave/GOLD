c2b55f26ea5209c64ac23aea2177deef263758f6
Mysura Reddy <Mysura Reddy@LAPTOP-6VNV3QUD>
2024-05-29 12:41:10 +0530

    Sticky Addtocart for Drybar US

--- src/main/java/TestComponent/Drybar_US/GoldDrybarUSHelper.java
+++ src/main/java/TestComponent/Drybar_US/GoldDrybarUSHelper.java
@@ -127,6 +127,145 @@ public class GoldDrybarUSHelper {
 		}
 	}
 	
+	public void Sticky_Add_to_Cart(String Dataset) {
+		// TODO Auto-generated method stub
+		String products = data.get(Dataset).get("Products");
+		String Productsize = data.get(Dataset).get("Size");
+	//	String productcolor = data.get(Dataset).get("Color");
+		System.out.println(products);
+
+		String results = Common.findElement("xpath", "//span[@id='algolia-srp-title']").getText();
+		System.out.println(results);
+		try {
+
+			if (results.contains("Atmos AG 50")) { // need to implement from the Header links After the configurations
+				Sync.waitPageLoad();
+				for (int i = 0; i <= 10; i++) {
+					Sync.waitElementPresent("xpath", "//img[contains(@class,'m-product-card__image')]");
+					List<WebElement> webelementslist = Common.findElements("xpath",
+							"//img[contains(@class,'m-product-card__image')]");
+
+					String s = webelementslist.get(i).getAttribute("src");
+					System.out.println(s);
+					if (s.isEmpty()) {
+
+					} else {
+						break;
+					}
+				}
+				Thread.sleep(6000);
+				Sync.waitElementPresent(30, "xpath", "//img[@alt='" + products + "']");
+				Common.clickElement("xpath", "//img[@alt='" + products + "']");
+				Sync.waitPageLoad();
+				Thread.sleep(4000);
+				Common.actionsKeyPress(Keys.END);
+				Thread.sleep(4000);
+			
+				Sync.waitElementPresent("xpath",
+						"//div[@class='sticky-atc__inner']//div[@data-option-label='" + Productsize + "']");
+				Common.clickElement("xpath",
+						"//div[@class='sticky-atc__inner']//div[@data-option-label='" + Productsize + "']");
+				Sync.waitElementPresent("xpath", "//button[@id='product-sticky-addtocart-button']");
+				Common.clickElement("xpath", "//button[@id='product-sticky-addtocart-button']");
+				Sync.waitPageLoad();
+				Thread.sleep(4000);
+				/* String message = Common.findElement("xpath", "//div[@data-ui-id='message-success']")
+						.getAttribute("data-ui-id");
+				System.out.println(message);
+				Common.assertionCheckwithReport(message.contains("success"), "validating the  product add to the cart",
+						"Product should be add to cart", "Sucessfully product added to the cart ",
+						"failed to add product to the cart"); */
+				Thread.sleep(4000);
+				Common.actionsKeyPress(Keys.HOME);
+				Common.actionsKeyPress(Keys.UP);
+
+			} else {
+				Sync.waitPageLoad();
+				for (int i = 0; i <= 10; i++) {
+					Sync.waitElementPresent("xpath", "//img[contains(@class,'m-product-card__image')]");
+					List<WebElement> webelementslist = Common.findElements("xpath",
+							"//img[contains(@class,'m-product-card__image')]");
+
+					String s = webelementslist.get(i).getAttribute("src");
+					System.out.println(s);
+					if (s.isEmpty()) {
+
+					} else {
+						break;
+					}
+				}
+				Thread.sleep(6000);
+				Sync.waitElementPresent(30, "xpath", "//img[@alt='" + products + "']");
+				Common.clickElement("xpath", "//img[@alt='" + products + "']");
+				Sync.waitPageLoad();
+				Common.actionsKeyPress(Keys.END);
+				Common.clickElement("xpath", "//button[@id='product-sticky-addtocart-button']");
+				Sync.waitPageLoad();
+				Thread.sleep(4000);
+			/*	Sync.waitElementPresent(30, "xpath", "//div[@data-ui-id='message-success']");
+				String message = Common.findElement("xpath", "//div[@data-ui-id='message-success']")
+						.getAttribute("data-ui-id");
+				System.out.println(message);
+				Common.assertionCheckwithReport(message.contains("success"), "validating the  product add to the cart",
+						"Product should be add to cart", "Sucessfully product added to the cart ",
+						"failed to add product to the cart"); */
+				Common.actionsKeyPress(Keys.HOME);
+			}
+		} catch (Exception | Error e) {
+			e.printStackTrace();
+			ExtenantReportUtils.addFailedLog("validating the  product add to the cart", "Product should be add to cart",
+					"unable to add product to the cart", Common.getscreenShot("failed to add product to the cart"));
+
+			Assert.fail();
+		}
+	}
+	public void HairProducts_headerlinks(String Dataset) {
+		// TODO Auto-generated method stub
+		String expectedResult = "User should click the" + Dataset;
+		String Conditioners = data.get(Dataset).get("Brushes");
+		
+		String header=data.get(Dataset).get("headers");
+		try {
+
+			Thread.sleep(3000);
+			Sync.waitElementPresent("xpath",
+					"//a[contains(@class,'level-top')]//span[contains(text(),'"+ header +"')]");
+			
+			Common.clickElement("xpath", "//a[contains(@class,'level-top')]//span[contains(text(),'" + header + "')]");
+
+			Thread.sleep(3000);
+
+			try {
+				Common.mouseOver("xpath", "//span[contains(text(),'"+ header +"')]");
+			} catch (Exception e) {
+				Common.clickElement("xpath", "//a[@class='level-top ui-corner-all']//span[text()='"+ header +"']");
+			}
+			Common.clickElement("xpath", "//span[contains(text(),'" + Conditioners + "')]");
+			Thread.sleep(4000);
+			
+			Sync.waitPageLoad();
+			Thread.sleep(6000);
+			expectedResult = "User should select the " + Dataset + "category";
+			int sizebotteles = Common.findElements("xpath", "//span[contains(text(),'"+ header +"')]").size();
+			Common.assertionCheckwithReport(sizebotteles > 0,
+					"validating the product category as" + Dataset + "from navigation menu ", expectedResult,
+					"Selected the " + Dataset + " category", "User unabel to click" + Dataset + "");
+
+		}
+
+		catch (Exception | Error e) {
+			e.printStackTrace();
+			ExtenantReportUtils.addFailedLog("validating the product category as" + Dataset + "from navigation menu ",
+					expectedResult, "Unable to Selected the " + Dataset + " category",
+					Common.getscreenShot("Failed to click on the" + Dataset + ""));
+
+			Assert.fail();
+		}
+
+	}
+
+
+	
 	public void RegaddDeliveryAddress(String dataSet) {
 		// TODO Auto-generated method stub
 		String expectedResult = "shipping address is entering in the fields";
