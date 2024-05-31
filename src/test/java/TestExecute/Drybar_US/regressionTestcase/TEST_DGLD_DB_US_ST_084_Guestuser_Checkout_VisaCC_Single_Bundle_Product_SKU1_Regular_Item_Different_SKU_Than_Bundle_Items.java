beb73fe8f63778a4cd5483981db5623255ddf80a
Rajkumar <rajkumarpotharaju1997@gmail.com>
2024-05-31 18:23:44 +0530

    For Drybar US commited 3 test cases.

---
+++ src/test/java/TestExecute/Drybar_US/regressionTestcase/TEST_DGLD_DB_US_ST_084_Guestuser_Checkout_VisaCC_Single_Bundle_Product_SKU1_Regular_Item_Different_SKU_Than_Bundle_Items.java
@@ -0,0 +1,57 @@
+package TestExecute.Drybar_US.regressionTestcase;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import TestComponent.Drybar_US.GoldDrybarUSHelper;
+import TestLib.Common;
+import TestLib.Login;
+
+public class TEST_DGLD_DB_US_ST_084_Guestuser_Checkout_VisaCC_Single_Bundle_Product_SKU1_Regular_Item_Different_SKU_Than_Bundle_Items {
+
+	String datafile = "Drybar_US//GoldDrybarTestData.xlsx";
+	GoldDrybarUSHelper Drybar = new GoldDrybarUSHelper(datafile,"Bundles");
+
+	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
+	public void Guestuser_Checkout_VisaCC_Single_Bundle_Product_SKU1_Regular_Item_Different_SKU_Than_Bundle_Items () throws Exception {
+
+		try {
+		
+			Drybar.Verify_Homepage();
+			Drybar.click_singinButton();
+			Drybar.login_Drybar("AccountDetails");
+			Drybar.search_product("Bundle Product");  
+			Drybar.Addtocart_Bundle("Bundle Product"); 
+			Drybar.HairTools_headerlinks("Hair Tools"); 
+			Drybar.addtocart("PLP Product");
+			Drybar.minicart_Checkout();
+			Drybar.RegaddDeliveryAddress("AccountDetails");
+			Drybar.selectshippingmethod("GroundShipping method");
+			Drybar.clickSubmitbutton_Shippingpage();
+			Drybar.updatePaymentAndSubmitOrder("PaymentDetails");
+
+		} catch (Exception e) {
+
+			Assert.fail(e.getMessage(), e);
+		}
+	}
+	
+	@AfterTest
+	public void clearBrowser() {
+		Common.closeAll();
+		
+
+	}
+
+	@BeforeTest
+	public void startTest() throws Exception {
+		System.setProperty("configFile", "Drybar_US\\config.properties");
+        Login.signIn();
+        Drybar.close_add();
+        
+
+	}
+
+}
