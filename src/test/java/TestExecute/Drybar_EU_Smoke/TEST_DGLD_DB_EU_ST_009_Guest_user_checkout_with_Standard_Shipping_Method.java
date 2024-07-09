cfe11c8bd403713d07356c8b6cfde97a944a6897
vivek <vivek@vivek>
2024-07-09 12:28:55 +0530

    Drybar-EU Required Folder's and Testdata Creared

---
+++ src/test/java/TestExecute/Drybar_EU_Smoke/TEST_DGLD_DB_EU_ST_009_Guest_user_checkout_with_Standard_Shipping_Method.java
@@ -0,0 +1,10 @@
+package TestExecute.Drybar_EU_Smoke;
+
+public class TEST_DGLD_DB_EU_ST_009_Guest_user_checkout_with_Standard_Shipping_Method {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
