94e1728d1b311003b73a2b82728e668dc42cfd19
vivek <vivek@vivek>
2024-07-10 14:03:32 +0530

    09 testcase changed helper

--- src/test/java/TestExecute/Drybar_EU_Smoke/TEST_DGLD_DB_EU_ST_009_Guest_user_checkout_with_Standard_Shipping_Method.java
+++ src/test/java/TestExecute/Drybar_EU_Smoke/TEST_DGLD_DB_EU_ST_009_Guest_user_checkout_with_Standard_Shipping_Method.java
@@ -5,7 +5,8 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import TestComponent.Drybar_EU.GoldDrybarEUHelper;
+
+import TestComponent.Drybar_US.GoldDrybarUSHelper;
 import TestLib.Common;
 import TestLib.Login;
 
@@ -14,7 +15,7 @@ public class TEST_DGLD_DB_EU_ST_009_Guest_user_checkout_with_Standard_Shipping_M
 
 		
 		String datafile = "Drybar_EU//GoldDrybarEUTestData.xlsx";
-		GoldDrybarEUHelper Drybar_EU = new GoldDrybarEUHelper(datafile,"DataSet");
+		GoldDrybarUSHelper Drybar_US= new GoldDrybarUSHelper(datafile,"DataSet");
 		
 		
 		@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
@@ -22,14 +23,14 @@ public class TEST_DGLD_DB_EU_ST_009_Guest_user_checkout_with_Standard_Shipping_M
 
 			try {
 			
-				Drybar_EU.Verify_Homepage();
-				Drybar_EU.HairTools_headerlinks("Hair Tools"); 
-				Drybar_EU.addtocart("PLP Product");
-				Drybar_EU.minicart_Checkout();
-				Drybar_EU.addDeliveryAddress_Guestuser("Address");
-				Drybar_EU.selectshippingmethod("GroundShipping method");
-				Drybar_EU.clickSubmitbutton_Shippingpage();
-				Drybar_EU.updatePaymentAndSubmitOrder("PaymentDetails");	
+				Drybar_US.Verify_Homepage();
+				Drybar_US.HairTools_headerlinks("Hair Tools"); 
+				Drybar_US.addtocart("PLP Product");
+				Drybar_US.minicart_Checkout();
+				Drybar_US.addDeliveryAddress_Guestuser("Address");
+				Drybar_US.selectshippingmethod("GroundShipping method");
+				Drybar_US.clickSubmitbutton_Shippingpage();
+				Drybar_US.updatePaymentAndSubmitOrder("PaymentDetails");	
 
 			} catch (Exception e) {
 
@@ -48,7 +49,7 @@ public class TEST_DGLD_DB_EU_ST_009_Guest_user_checkout_with_Standard_Shipping_M
 		public void startTest() throws Exception {
 			System.setProperty("configFile", "Drybar_EU\\config.properties");
 	        Login.signIn();
-	        Drybar_EU.close_add();
+	        Drybar_US.close_add();
 	        
 
 		}
