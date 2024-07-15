8f502ea911cd843c4adc07e06e73f5f6983c3303
avayugundla <avayugundla@CPC-avayu-3AUJR.helenoftroy.com>
2024-07-15 16:11:30 +0530

    shipping method changes

--- src/main/java/TestComponent/Drybar_US/GoldDrybarusHelper2.java
+++ src/main/java/TestComponent/Drybar_US/GoldDrybarusHelper2.java
@@ -859,13 +859,13 @@ public class GoldDrybarusHelper2 {
 
 		try {
 			Thread.sleep(5000);
-			int size = Common.findElements("xpath", "//label[@class='a-radio-button__label']").size();
+			int size = Common.findElements("xpath", "//label[contains(@for,'shipping-method')]").size();
 			System.out.println(size);
 			if (size > 0  ) {
 				
 				Thread.sleep(4000);
-				Sync.waitElementPresent("xpath", "//td[text()='"+ method +"']");
-				Common.clickElement("xpath", "//td[text()='"+ method +"']");
+				Sync.waitElementPresent("xpath", "//div[contains(text(),'"+ method +"')]");
+				Common.clickElement("xpath", "//div[contains(text(),'"+ method +"')]");
 			}
 			else
 			{
