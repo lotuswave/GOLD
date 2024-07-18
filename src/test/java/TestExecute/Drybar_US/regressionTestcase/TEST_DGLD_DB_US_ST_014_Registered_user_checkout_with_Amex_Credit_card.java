805a9f742c8f788d3783926e9ea0c958831cc708
avayugundla <avayugundla@CPC-avayu-3AUJR.helenoftroy.com>
2024-07-18 12:24:17 +0530

    14 testcase changes for drybar us

--- src/test/java/TestExecute/Drybar_US/regressionTestcase/TEST_DGLD_DB_US_ST_014_Registered_user_checkout_with_Amex_Credit_card.java
+++ src/test/java/TestExecute/Drybar_US/regressionTestcase/TEST_DGLD_DB_US_ST_014_Registered_user_checkout_with_Amex_Credit_card.java
@@ -5,14 +5,14 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import TestComponent.Drybar_US.GoldDrybarUSHelper;
+import TestComponent.Drybar_US.GoldDrybarusHelper2;
 import TestLib.Common;
 import TestLib.Login;
 
 public class TEST_DGLD_DB_US_ST_014_Registered_user_checkout_with_Amex_Credit_card {
 
 	String datafile = "Drybar_US//GoldDrybarTestData.xlsx";
-	GoldDrybarUSHelper Drybar = new GoldDrybarUSHelper(datafile,"DataSet");
+	GoldDrybarusHelper2 Drybar = new GoldDrybarusHelper2(datafile,"DataSet");
 
 	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
 	public void Validate_Registered_User_Checkout_Funtionality_Amex_card () throws Exception {
@@ -27,7 +27,7 @@ public class TEST_DGLD_DB_US_ST_014_Registered_user_checkout_with_Amex_Credit_ca
 			Drybar.minicart_Checkout();
 			Drybar.RegaddDeliveryAddress("AccountDetails");
 			Drybar.selectshippingmethod("GroundShipping method");
-			Drybar.clickSubmitbutton_Shippingpage();
+//			Drybar.clickSubmitbutton_Shippingpage();
 			Drybar.updatePaymentAndSubmitOrder("CCAmexcard");
 			
 
