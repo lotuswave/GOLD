2e93ce343342987a3c3ab5f907d06c598d81c7ba
avayugundla <avayugundla@CPC-avayu-3AUJR.helenoftroy.com>
2024-07-19 10:53:56 +0530

    78 and 79 tescases for drybar us

--- src/test/java/TestExecute/Drybar_US/regressionTestcase/TEST_DGLD_DB_US_ST_079_Validate_RegisterUser_Checkout_Aerosol_Products_and_NonAerosol_Products_with_Non_POBox_and_Hawaii_no_Shipping_Methods.java
+++ src/test/java/TestExecute/Drybar_US/regressionTestcase/TEST_DGLD_DB_US_ST_079_Validate_RegisterUser_Checkout_Aerosol_Products_and_NonAerosol_Products_with_Non_POBox_and_Hawaii_no_Shipping_Methods.java
@@ -5,14 +5,14 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import TestComponent.Drybar_US.GoldDrybarUSHelper;
+import TestComponent.Drybar_US.GoldDrybarusHelper2;
 import TestLib.Common;
 import TestLib.Login;
 
 public class TEST_DGLD_DB_US_ST_079_Validate_RegisterUser_Checkout_Aerosol_Products_and_NonAerosol_Products_with_Non_POBox_and_Hawaii_no_Shipping_Methods {
 
 	String datafile = "Drybar_US//GoldDrybarTestData.xlsx";
-	GoldDrybarUSHelper Drybar = new GoldDrybarUSHelper(datafile,"DataSet");
+	GoldDrybarusHelper2 Drybar = new GoldDrybarusHelper2(datafile,"DataSet");
 
 	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
 	public void Validate_RegisterUser_Checkout_Aerosol_Products_and_NonAerosol_Products_with_Non_POBox_and_Hawaii_no_Shipping_Methods () throws Exception {
@@ -40,7 +40,7 @@ public class TEST_DGLD_DB_US_ST_079_Validate_RegisterUser_Checkout_Aerosol_Produ
 	
 	@AfterTest
 	public void clearBrowser() {
-		Common.closeAll();
+//		Common.closeAll();
 
 	}
 
