8b40539513d767c39689338734b80b7e89c6564f
avayugundla <avayugundla@CPC-avayu-3AUJR.helenoftroy.com>
2024-07-22 13:23:45 +0530

    144 TESTCASE for drybar us

--- src/test/java/TestExecute/Drybar_US/regressionTestcase/TEST_DGLD_DB_US_ST_144_Add_to_Cart_from_Sticky_Cart_in_PDP_Page.java
+++ src/test/java/TestExecute/Drybar_US/regressionTestcase/TEST_DGLD_DB_US_ST_144_Add_to_Cart_from_Sticky_Cart_in_PDP_Page.java
@@ -5,14 +5,14 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import TestComponent.Drybar_US.GoldDrybarUSHelper;
+import TestComponent.Drybar_US.GoldDrybarusHelper2;
 import TestLib.Common;
 import TestLib.Login;
 
 public class TEST_DGLD_DB_US_ST_144_Add_to_Cart_from_Sticky_Cart_in_PDP_Page {
 
 	String datafile = "Drybar_US//GoldDrybarTestData.xlsx";
-	GoldDrybarUSHelper Drybar = new GoldDrybarUSHelper(datafile,"DataSet");
+	GoldDrybarusHelper2 Drybar = new GoldDrybarusHelper2(datafile,"DataSet");
 
 	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
 	public void Validate_Guest_user_Add_to_Cart_from_Sticky_Cart_in_PDP_Page () throws Exception {
