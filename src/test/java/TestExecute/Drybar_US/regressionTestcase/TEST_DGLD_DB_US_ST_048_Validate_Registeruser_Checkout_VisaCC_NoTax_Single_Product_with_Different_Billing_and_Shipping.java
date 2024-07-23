21365958916695f15cf2909b8ec070f56df11fc0
avayugundla <avayugundla@CPC-avayu-3AUJR.helenoftroy.com>
2024-07-23 12:47:34 +0530

    48 TESTCASE FOR DRYBAR US

--- src/test/java/TestExecute/Drybar_US/regressionTestcase/TEST_DGLD_DB_US_ST_048_Validate_Registeruser_Checkout_VisaCC_NoTax_Single_Product_with_Different_Billing_and_Shipping.java
+++ src/test/java/TestExecute/Drybar_US/regressionTestcase/TEST_DGLD_DB_US_ST_048_Validate_Registeruser_Checkout_VisaCC_NoTax_Single_Product_with_Different_Billing_and_Shipping.java
@@ -23,8 +23,8 @@ public class TEST_DGLD_DB_US_ST_048_Validate_Registeruser_Checkout_VisaCC_NoTax_
 			Drybar.Verify_Homepage();
 			Drybar.click_singinButton();
 			Drybar.login_Drybar("AccountDetails");
-			//Drybar.HairTools_headerlinks("Hair Tools"); 
-			//Drybar.addtocart("PLP Product");
+			Drybar.HairTools_headerlinks("Hair Tools"); 
+			Drybar.addtocart("PLP Product");
 			Drybar.minicart_Checkout();
 			Drybar.RegaddDeliveryAddress("NoTaxAddress");
 			Drybar.selectshippingmethod("GroundShipping method");
@@ -42,7 +42,7 @@ public class TEST_DGLD_DB_US_ST_048_Validate_Registeruser_Checkout_VisaCC_NoTax_
 	
 	@AfterTest
 	public void clearBrowser() {
-		//Common.closeAll();
+		Common.closeAll();
 		
 
 	}
