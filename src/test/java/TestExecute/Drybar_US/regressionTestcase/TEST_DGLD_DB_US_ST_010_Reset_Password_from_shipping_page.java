ae3c64d9729bb37d7998964f743d350be9046ba7
skasarla <skasarla@CPC-skasa-DD8QS.helenoftroy.com>
2024-07-26 15:37:20 +0530

    010 testcase change

--- src/test/java/TestExecute/Drybar_US/regressionTestcase/TEST_DGLD_DB_US_ST_010_Reset_Password_from_shipping_page.java
+++ src/test/java/TestExecute/Drybar_US/regressionTestcase/TEST_DGLD_DB_US_ST_010_Reset_Password_from_shipping_page.java
@@ -6,13 +6,14 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import TestComponent.Drybar_US.GoldDrybarUSHelper;
+import TestComponent.Drybar_US.GoldDrybarusHelper2;
 import TestLib.Common;
 import TestLib.Login;
 
 public class TEST_DGLD_DB_US_ST_010_Reset_Password_from_shipping_page {
 
 	String datafile = "Drybar_US//GoldDrybarTestData.xlsx";
-	GoldDrybarUSHelper Drybar = new GoldDrybarUSHelper(datafile,"DataSet");
+	GoldDrybarusHelper2 Drybar = new GoldDrybarusHelper2(datafile,"DataSet");
 
 	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
 	public void Validate_Guest_user_checkout_with_Expedited_shipping_method () throws Exception {
@@ -25,7 +26,8 @@ public class TEST_DGLD_DB_US_ST_010_Reset_Password_from_shipping_page {
 			Drybar.HairTools_headerlinks("Hair Tools");
 			Drybar.addtocart("PLP Product");
 			Drybar.minicart_Checkout();
-			Drybar.Shipping_Forgot_Password("AccountDetails");
+			//Drybar.Shipping_Forgot_Password("AccountDetails");
+			Drybar.click_singin_Shippingpage();
 			Drybar.Forgot_password("AccountDetails");
 			
 
