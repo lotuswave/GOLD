a867f8f3e1e79e1aa63c931b61bf68f417f09659
avayugundla <avayugundla@CPC-avayu-3AUJR.helenoftroy.com>
2024-07-30 11:28:36 +0530

    163 testcase changes for drybar us

--- src/main/java/TestComponent/Drybar_US/GoldDrybarusHelper2.java
+++ src/main/java/TestComponent/Drybar_US/GoldDrybarusHelper2.java
@@ -9862,6 +9862,66 @@ public void close_successmessage() {
 	}
 }
 
+public void webpagelinks_validation(String Dataset) throws Exception, IOException {
+	// TODO Auto-generated method stub
+	
+	String links = data.get(Dataset).get("Links");
+	int j = 0;
+
+	String[] strArray = links.split("\\r?\\n");
+	for (int i = 0; i < strArray.length; i++) {
+		System.out.println(strArray[i]);
+
+		if (Common.getCurrentURL().contains("stage4")) {
+
+			Common.oppenURL((strArray[i]));
+			Thread.sleep(4000);
+			int responcecode = getpageresponce(Common.getCurrentURL());
+			System.out.println(responcecode);
+
+			if (responcecode == 200) {
+				ExtenantReportUtils.addPassLog("Validating Page URL ", "page configured with products ",
+						"successfully page configured with products",
+						Common.getscreenShotPathforReport("link" + i));
+			} else {
+
+				j++;
+
+				ExtenantReportUtils.addFailedLog("Validating Page URL  " + Common.getCurrentURL(),
+						"page configured with products ", "unable to find page it showing 404 error",
+						Common.getscreenShotPathforReport("link" + i));
+
+			}
+
+		} else if (Common.getCurrentURL().contains("https://mcloud-na-stage3.drybar.com/")) {
+
+			Common.oppenURL(strArray[i].replace("mcloud-na-stage3", "www"));
+
+			int responcecode = getpageresponce(Common.getCurrentURL());
+			System.out.println(responcecode);
+
+			if (responcecode == 200) {
+				ExtenantReportUtils.addPassLog("Validating Page URL ", "page configured with products ",
+						"successfully page configured with products",
+						Common.getscreenShotPathforReport("link" + i));
+			} else {
+
+				j++;
+
+				ExtenantReportUtils.addFailedLog("Validating Page URL  " + Common.getCurrentURL(),
+						"page configured with products ", "unable to find page it showing 40 error",
+						Common.getscreenShotPathforReport("link" + i));
+
+			}
+		}
+	}
+
+	if (j > 1) {
+		Assert.fail();
+	}
+
+}
+
 }
 
 
