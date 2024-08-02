23d86c6295a4e6ca7774024411b4ab32b25c4200
avayugundla <avayugundla@CPC-avayu-3AUJR.helenoftroy.com>
2024-08-02 16:13:12 +0530

    175 testcase for drybar us

--- src/test/java/TestExecute/Drybar_US/regressionTestcase/TEST_DGLD_DB_US_ST_175_Register_user_Checkout_With_simple_bundle_configurble_subcribtion_prodcut_using_CC.java
+++ src/test/java/TestExecute/Drybar_US/regressionTestcase/TEST_DGLD_DB_US_ST_175_Register_user_Checkout_With_simple_bundle_configurble_subcribtion_prodcut_using_CC.java
@@ -5,14 +5,14 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import TestComponent.Drybar_US.GoldDrybarUSHelper;
+import TestComponent.Drybar_US.GoldDrybarusHelper2;
 import TestLib.Common;
 import TestLib.Login;
 
 public class TEST_DGLD_DB_US_ST_175_Register_user_Checkout_With_simple_bundle_configurble_subcribtion_prodcut_using_CC {
 
 	String datafile = "Drybar_US//GoldDrybarTestData.xlsx";
-	GoldDrybarUSHelper Drybar = new GoldDrybarUSHelper(datafile,"Bundles");
+	GoldDrybarusHelper2 Drybar = new GoldDrybarusHelper2(datafile,"Bundles");
 
 	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
 	public void Validate_Register_user_Checkout_With_simple_bundle_configurble_subcribtion_prodcut_using_CC () throws Exception {
