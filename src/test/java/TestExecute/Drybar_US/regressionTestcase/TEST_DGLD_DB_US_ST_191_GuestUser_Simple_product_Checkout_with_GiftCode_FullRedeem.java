0699f3b72af706671985b924f9e04d9b8a0c1847
skasarla <skasarla@CPC-skasa-DD8QS.helenoftroy.com>
2024-08-07 22:05:41 +0530

    Dryus-added 191 testcase

---
+++ src/test/java/TestExecute/Drybar_US/regressionTestcase/TEST_DGLD_DB_US_ST_191_GuestUser_Simple_product_Checkout_with_GiftCode_FullRedeem.java
@@ -0,0 +1,53 @@
+package TestExecute.Drybar_US.regressionTestcase;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import TestComponent.Drybar_US.GoldDrybarusHelper2;
+import TestLib.Login;
+
+public class TEST_DGLD_DB_US_ST_191_GuestUser_Simple_product_Checkout_with_GiftCode_FullRedeem {
+	
+	String datafile = "Drybar_US//GoldDrybarTestData.xlsx";
+	GoldDrybarusHelper2 Drybar = new GoldDrybarusHelper2(datafile,"DataSet");
+
+	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
+	public void Validate_GuestUser_Simple_product_Checkout_with_GiftCode_FullRedeem () throws Exception {
+
+		try {
+		
+			Drybar.Verify_Homepage();
+			Drybar.HairTools_headerlinks("Hair Tools"); 
+			Drybar.addtocart("PLP Product");
+			Drybar.minicart_Checkout();
+			Drybar.addDeliveryAddress_Guestuser("Address");
+			Drybar.selectshippingmethod("GroundShipping method");
+			Drybar.clickSubmitbutton_Shippingpage();
+			Drybar.gitCard("GiftCode");
+			Drybar.giftCardSubmitOrder();
+			
+
+		} catch (Exception e) {
+
+			Assert.fail(e.getMessage(), e);
+		}
+	}
+	
+	@AfterTest
+	public void clearBrowser() {
+//		Common.closeAll();
+
+	}
+
+	@BeforeTest
+	public void startTest() throws Exception {
+		System.setProperty("configFile", "Drybar_US\\config.properties");
+        Login.signIn();
+        Drybar.close_add();
+        
+
+	}
+
+}
