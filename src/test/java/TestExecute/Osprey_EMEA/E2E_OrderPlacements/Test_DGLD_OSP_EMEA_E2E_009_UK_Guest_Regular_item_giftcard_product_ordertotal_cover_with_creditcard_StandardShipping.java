8b664e7bd79c9263f36c2252dc16b30781d8ab01
vivek <vivek@vivek>
2024-09-05 13:39:22 +0530

    009 testcase scenario Update

--- src/test/java/TestExecute/Osprey_EMEA/E2E_OrderPlacements/Test_DGLD_OSP_EMEA_E2E_009_UK_Guest_Regular_item_giftcard_product_ordertotal_cover_with_creditcard_StandardShipping.java
+++ src/test/java/TestExecute/Osprey_EMEA/E2E_OrderPlacements/Test_DGLD_OSP_EMEA_E2E_009_UK_Guest_Regular_item_giftcard_product_ordertotal_cover_with_creditcard_StandardShipping.java
@@ -10,7 +10,7 @@ import TestComponent.Osprey_EMEA.OspreyRegressionEMEA;
 import TestLib.Common;
 import TestLib.Login;
 
-public class Test_DGLD_OSP_EMEA_E2E_009_Guest_Regular_item_giftcard_product_ordertotal_cover_with_creditcard_StandardShipping {
+public class Test_DGLD_OSP_EMEA_E2E_009_UK_Guest_Regular_item_giftcard_product_ordertotal_cover_with_creditcard_StandardShipping {
 
 	String datafile = "Osprey_EMEA//OSPEMEA_E2E_orderDetails.xlsx";
 	OspreyEMEA_E2EHelper Osprey_ReEu = new OspreyEMEA_E2EHelper(datafile,"Checkout payments");
@@ -23,6 +23,9 @@ public class Test_DGLD_OSP_EMEA_E2E_009_Guest_Regular_item_giftcard_product_orde
 			Osprey_ReEu.prepareOrdersData("OspreyEU_E2E_orderDetails.xlsx");
 			String Description = "UK store -1 REG + 1 GC PURCHASE + CC";
 			Osprey_ReEu.verifingHomePage();
+			Osprey_ReEu.search_product("Giftcard");
+			Osprey_ReEu.Gift_cards("Giftcard");
+			Osprey_ReEu.Card_Value("price");
 			Osprey_ReEu.search_product("Product");
 			Osprey_ReEu.simple_addtocart("Product");
 			Osprey_ReEu.minicart_Checkout();
