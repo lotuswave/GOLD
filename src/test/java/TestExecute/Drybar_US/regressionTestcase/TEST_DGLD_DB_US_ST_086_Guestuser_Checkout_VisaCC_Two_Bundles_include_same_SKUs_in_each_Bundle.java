7daac297e9ef2552457843ad46d75efa9977c500
avayugundla <avayugundla@CPC-avayu-3AUJR.helenoftroy.com>
2024-09-12 17:06:04 +0530

    86 testcase changed to the guest user changes done for the drybar us

--- src/test/java/TestExecute/Drybar_US/regressionTestcase/TEST_DGLD_DB_US_ST_086_Guestuser_Checkout_VisaCC_Two_Bundles_include_same_SKUs_in_each_Bundle.java
+++ src/test/java/TestExecute/Drybar_US/regressionTestcase/TEST_DGLD_DB_US_ST_086_Guestuser_Checkout_VisaCC_Two_Bundles_include_same_SKUs_in_each_Bundle.java
@@ -5,14 +5,14 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import TestComponent.Drybar_US.GoldDrybarUSHelper;
+import TestComponent.Drybar_US.GoldDrybarusHelper2;
 import TestLib.Common;
 import TestLib.Login;
 
 public class TEST_DGLD_DB_US_ST_086_Guestuser_Checkout_VisaCC_Two_Bundles_include_same_SKUs_in_each_Bundle {
 
 	String datafile = "Drybar_US//GoldDrybarTestData.xlsx";
-	GoldDrybarUSHelper Drybar = new GoldDrybarUSHelper(datafile,"Bundles");
+	GoldDrybarusHelper2 Drybar = new GoldDrybarusHelper2(datafile,"Bundles");
 
 	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
 	public void Validate_Guestuser_Checkout_VisaCC_Two_Bundles_include_same_SKUs_in_each_Bundle () throws Exception {
@@ -20,14 +20,12 @@ public class TEST_DGLD_DB_US_ST_086_Guestuser_Checkout_VisaCC_Two_Bundles_includ
 		try {
 		
 			Drybar.Verify_Homepage();
-			Drybar.click_singinButton();
-			Drybar.login_Drybar("AccountDetails");
 			Drybar.search_product("Bundle 1");  
 			Drybar.Addtocart_Bundle("Bundle 1"); 
 			Drybar.search_product("Bundle 2");  
 			Drybar.Addtocart_Bundle("Bundle 2");
 			Drybar.minicart_Checkout();
-			Drybar.RegaddDeliveryAddress("AccountDetails");
+			Drybar.addDeliveryAddress_Guestuser("Address");
 			Drybar.selectshippingmethod("GroundShipping method");
 			Drybar.clickSubmitbutton_Shippingpage();
 			Drybar.updatePaymentAndSubmitOrder("PaymentDetails");
