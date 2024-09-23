b89513b4c1f87def194f0bdc3595c1c47be7d6b9
vnarra <vnarra@CPC-vnarr-42JDJ.helenoftroy.com>
2024-09-23 16:30:05 +0530

    DB_E2E_007

--- src/test/java/TestExecute/Drybar_US/Drybar_O2C_E2E/Test_DGLD_DB_E2E_007_Guestuser_checkout_3items_ExpeditedShipping_GiftCode_RedeeemPartial_CC.java
+++ src/test/java/TestExecute/Drybar_US/Drybar_O2C_E2E/Test_DGLD_DB_E2E_007_Guestuser_checkout_3items_ExpeditedShipping_GiftCode_RedeeemPartial_CC.java
@@ -11,37 +11,37 @@ import TestComponent.Drybar_US.GoldDrybarusE2EHelper;
 import TestLib.Common;
 import TestLib.Login;
 
-public class Test_DGLD_DB_E2E_002_Registeruser_checkout_with_MultipleItems_standard_Shiping_CCMaster_PO_Box_address {
+public class Test_DGLD_DB_E2E_007_Guestuser_checkout_3items_ExpeditedShipping_GiftCode_RedeeemPartial_CC {
 
 	String datafile = "Drybar_US//GoldDrybarTestData.xlsx";
 	GoldDrybarusE2EHelper Drybar = new GoldDrybarusE2EHelper(datafile,"Drybar_E2E");;
 
 	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
-	public void Verifying_RegisterUser_checkout_AfterPay_Payment_Standard_shipping () throws Exception {
+	public void Verifying_Guestuser_checkout_3items_Expedited_shipping_GiftCode_RedeeemPartial () throws Exception {
 
 		try {
 		Drybar.prepareOrdersData("Drybar_E2E_orderDetails.xlsx");
-		String Description="Register user checkout with Multiple Items + standard Shiping +  CC Master + P.O Box address";
+		String Description="Guest user checkout with 3 items + Expedited shipping + Gift Code redeeem Partial + CC";
 		Drybar.Verify_Homepage();
-        Drybar.click_singinButton();
-        Drybar.login_Drybar("AccountDetails");
-        Drybar.search_product("900-0700-4 Product");
+		Drybar.search_product("900-0700-4 Product");
         Drybar.addtocart("900-0700-4 Product");
         Drybar.search_product("900-2230-1 Product");
         Drybar.addtocart("900-2230-1 Product");
         Drybar.search_product("900-0630-1 Product");
         Drybar.addtocart("900-0630-1 Product");
         Drybar.minicart_Checkout();
-        Drybar.RegaddDeliveryAddress("PO Box Address");
-        Drybar.selectshippingmethod("GroundShipping method");
-        String Used_GiftCode_No = ""; // Indicating no gift code used  
+        String Used_GiftCode= Drybar.Gift_Card_Enter("DRY-PRPD-TEST-QA");
+        Drybar.addDeliveryAddress_Guestuser("Address");
+        Drybar.selectshippingmethod("ExpeditedShipping method");
         Drybar.clickSubmitbutton_Shippingpage();
         String OrderNumber=Drybar.updatePaymentAndSubmitOrder("CCMastercard");
         Drybar.Admin_signin("AccountDetails");
         Drybar.click_Sales();
         HashMap<String, String> Orderstatus1= Drybar.order_verfication(OrderNumber);
-        Drybar.writeOrderNumber(Description,OrderNumber,Orderstatus1.get("Skus"),Orderstatus1.get("AdminOrderstatus"),Used_GiftCode_No);
+        Drybar.writeOrderNumber(Description,OrderNumber,Orderstatus1.get("Skus"),Orderstatus1.get("AdminOrderstatus"),Used_GiftCode);
         
+        
+       
 		} catch (Exception e) {
 
 			Assert.fail(e.getMessage(), e);
