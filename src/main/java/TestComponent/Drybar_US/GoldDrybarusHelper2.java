d5a484882491fc8c66f193a2e93a30f14149022e
skasarla <skasarla@CPC-skasa-DD8QS.helenoftroy.com>
2024-09-24 13:10:45 +0530

    DRYUS Helper2 changes 68 and 60

--- src/main/java/TestComponent/Drybar_US/GoldDrybarusHelper2.java
+++ src/main/java/TestComponent/Drybar_US/GoldDrybarusHelper2.java
@@ -3679,7 +3679,8 @@ public class GoldDrybarusHelper2 {
 			Sync.waitElementPresent("xpath", "//button[@aria-label='Close minicart']");
 			Common.clickElement("xpath", "//button[@aria-label='Close minicart']");
 			Thread.sleep(4000);
-			String originalprice = Common.getText("xpath", "//div[contains(@class,'old-price')]//span[@class='price line-through']").replace("$", "");
+			String originalprice = Common.getText("xpath", "//span[@class='price line-through hf:font-bold md:hf:font-normal']").replace("$", "");
+			//String originalprice = Common.getText("xpath", "//div[contains(@class,'old-price')]//span[@class='price line-through']").replace("$", "");
 			Float originalvalue = Float.parseFloat(originalprice);
 			String Newprice = Common.getText("xpath", "(//span[@class='price-wrapper']//span[@class='price'])").replace("$", "");
 			Float pricevalue = Float.parseFloat(Newprice);
@@ -5391,7 +5392,10 @@ public void FUll_Payment(String dataSet) {
 					// Sync.waitElementPresent(30, "xpath", "//td[contains(text(),'" + method +
 					// "')]");
 					String method1=Common.findElement("xpath", "//div[@class='pl-3 pr-1']").getText();
-					String shipping1= Common.findElement("xpath", "//span[@class='price-including-tax']").getText();
+					Thread.sleep(2000);
+					String shipping1= Common.findElement("xpath", "//span[@class='flex items-center']").getText();
+					
+					//String shipping1= Common.findElement("xpath", "//span[@class='price-including-tax']").getText();
 					//String method2=Common.findElement("xpath", "//td[@id='label_method_amstrates4_amstrates-label_carrier_amstrates4_amstrates']").getText();
 					//String shipping2= Common.findElement("xpath", "(//span[@class='shipping-method__radio'])[2]").getText();
 					
