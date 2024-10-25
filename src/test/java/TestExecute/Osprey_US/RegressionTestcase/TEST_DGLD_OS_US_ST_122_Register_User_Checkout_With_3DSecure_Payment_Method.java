2d518c2efcbd54875212b7a3af27b10e73dd73c2
avayugundla <avayugundla@CPC-avayu-3AUJR.helenoftroy.com>
2024-10-25 13:22:43 +0530

    122 testcase for osprey us

--- src/test/java/TestExecute/Osprey_US/RegressionTestcase/TEST_DGLD_OS_US_ST_122_Register_User_Checkout_With_3DSecure_Payment_Method.java
+++ src/test/java/TestExecute/Osprey_US/RegressionTestcase/TEST_DGLD_OS_US_ST_122_Register_User_Checkout_With_3DSecure_Payment_Method.java
@@ -5,14 +5,14 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import TestComponent.Osprey_EMEA.OspreyRegressionEMEA;
+import TestComponent.Osprey_US.GoldOspreyUSHyvaHelper;
 import TestLib.Common;
 import TestLib.Login;
 
 public class TEST_DGLD_OS_US_ST_122_Register_User_Checkout_With_3DSecure_Payment_Method {
 
 	String datafile = "Osprey_US//GoldOspreyus.xlsx";
-	OspreyRegressionEMEA Osprey_ReEu = new OspreyRegressionEMEA(datafile,"Checkout payments");
+	GoldOspreyUSHyvaHelper Osprey_ReEu = new GoldOspreyUSHyvaHelper(datafile,"Checkout payments");
 
 	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
 	public void Verifying_RegisterUser_StoreCredit_Checkout_Partial_with_AfterPay_and_valdiate_the_StoreCredit_balance_in_MY_Account_page () throws Exception {
