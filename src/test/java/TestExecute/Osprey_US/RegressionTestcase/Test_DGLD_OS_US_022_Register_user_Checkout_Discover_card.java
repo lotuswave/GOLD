f48265a43d99eafa33d9de1f9fe111051d26ed7f
avayugundla <avayugundla@CPC-avayu-3AUJR.helenoftroy.com>
2024-10-31 11:55:07 +0530

    22 testcase for osprey us

--- src/test/java/TestExecute/Osprey_US/RegressionTestcase/Test_DGLD_OS_US_022_Register_user_Checkout_Discover_card.java
+++ src/test/java/TestExecute/Osprey_US/RegressionTestcase/Test_DGLD_OS_US_022_Register_user_Checkout_Discover_card.java
@@ -5,14 +5,14 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import TestComponent.Osprey_EMEA.OspreyRegressionEMEA;
+import TestComponent.Osprey_US.GoldOspreyUSHyvaHelper;
 import TestLib.Common;
 import TestLib.Login;
 
 public class Test_DGLD_OS_US_022_Register_user_Checkout_Discover_card {
 
 	String datafile = "Osprey_US//GoldOspreyus.xlsx";
-	OspreyRegressionEMEA Osprey_ReEu = new OspreyRegressionEMEA(datafile,"Checkout payments");
+	GoldOspreyUSHyvaHelper Osprey_ReEu = new GoldOspreyUSHyvaHelper(datafile,"Checkout payments");
 
 	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
 	public void Verifying_Register_user_Checkout_Discover_card () throws Exception {
