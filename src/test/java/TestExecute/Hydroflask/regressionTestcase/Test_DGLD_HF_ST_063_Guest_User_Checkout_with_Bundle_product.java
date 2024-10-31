aaa46bac53e70ea4e3f3a58145d04c4df3ab0630
avayugundla <avayugundla@CPC-avayu-3AUJR.helenoftroy.com>
2024-10-31 11:35:28 +0530

    63 testcase for hydroflask us

--- src/test/java/TestExecute/Hydroflask/regressionTestcase/Test_DGLD_HF_ST_063_Guest_User_Checkout_with_Bundle_product.java
+++ src/test/java/TestExecute/Hydroflask/regressionTestcase/Test_DGLD_HF_ST_063_Guest_User_Checkout_with_Bundle_product.java
@@ -5,14 +5,14 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import TestComponent.Hydroflask.GoldHydroHelper;
+import TestComponent.Hydroflask.GoldHydroHyvaHelper;
 import TestLib.Common;
 import TestLib.Login;
 
 public class Test_DGLD_HF_ST_063_Guest_User_Checkout_with_Bundle_product {
 
 	String datafile = "Hydroflask//GoldHydroTestData.xlsx";
-	GoldHydroHelper Hydro = new GoldHydroHelper(datafile,"Bundle");
+	GoldHydroHyvaHelper Hydro = new GoldHydroHyvaHelper(datafile,"Bundle");
 
 	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
 	public void Validate_Guest_User_Checkout_with_Bundle_product () throws Exception {
