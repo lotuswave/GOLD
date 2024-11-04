fe70279c1bcebf8f98a007c8ba101da8cdcaba3c
avayugundla <avayugundla@CPC-avayu-3AUJR.helenoftroy.com>
2024-11-04 17:29:43 +0530

    84 testcase changes for oxo

--- src/test/java/TestExecute/OXO/regressionTestcase/Test_DGLD_OXO_ST_084_Checkout_with_GuestUser_Simple_Configurable_Bundle_Paypal.java
+++ src/test/java/TestExecute/OXO/regressionTestcase/Test_DGLD_OXO_ST_084_Checkout_with_GuestUser_Simple_Configurable_Bundle_Paypal.java
@@ -5,13 +5,13 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import TestComponent.OXO.GoldOxoHelper;
+import TestComponent.OXO.GoldOxoHyvaHelper;
 import TestLib.Common;
 import TestLib.Login;
 
 public class Test_DGLD_OXO_ST_084_Checkout_with_GuestUser_Simple_Configurable_Bundle_Paypal {
 	String datafile = "OXO//GoldOxoTestData.xlsx";	
-	GoldOxoHelper Oxo=new GoldOxoHelper(datafile,"DataSet");
+	GoldOxoHyvaHelper Oxo=new GoldOxoHyvaHelper(datafile,"DataSet");
 	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
 	public void Validate_GuestUser_Simple_Configurable_Bundle_Paypal() throws Exception {
 
