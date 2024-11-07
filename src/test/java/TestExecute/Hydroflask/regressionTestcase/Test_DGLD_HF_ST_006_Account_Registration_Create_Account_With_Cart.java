60107b573506195af73913574a744bea310c6675
avayugundla <avayugundla@CPC-avayu-3AUJR.helenoftroy.com>
2024-11-07 17:46:11 +0530

    6 testcase for hydroflask US

--- src/test/java/TestExecute/Hydroflask/regressionTestcase/Test_DGLD_HF_ST_006_Account_Registration_Create_Account_With_Cart.java
+++ src/test/java/TestExecute/Hydroflask/regressionTestcase/Test_DGLD_HF_ST_006_Account_Registration_Create_Account_With_Cart.java
@@ -5,14 +5,14 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import TestComponent.Hydroflask.GoldHydroHelper;
+import TestComponent.Hydroflask.GoldHydroHyvaHelper;
 import TestLib.Common;
 import TestLib.Login;
 
 public class Test_DGLD_HF_ST_006_Account_Registration_Create_Account_With_Cart {
 
 	String datafile = "Hydroflask//GoldHydroTestData.xlsx";
-	GoldHydroHelper Hydro = new GoldHydroHelper(datafile,"DataSet");
+	GoldHydroHyvaHelper Hydro = new GoldHydroHyvaHelper(datafile,"DataSet");
 
 	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
 	public void Validate_Account_Registration_Create_Account_With_Cart () throws Exception {
@@ -26,7 +26,7 @@ public class Test_DGLD_HF_ST_006_Account_Registration_Create_Account_With_Cart {
 			Hydro.MyFavorites_Guestuser("Product");
 			String minicart=Hydro.minicart_items();
 			Hydro.click_Createaccount();
-			Hydro.create_account_with_fav("AccountDetails");
+			Hydro.create_account("New Account Details");
 			Hydro.minicart_products(minicart);
 			
 
