a583a42adebadd9dce5d113e16e3f824d7e4f653
avayugundla <avayugundla@CPC-avayu-3AUJR.helenoftroy.com>
2024-11-08 16:31:42 +0530

    48 testcase for Hydroflask us

--- src/test/java/TestExecute/Hydroflask/regressionTestcase/Test_DGLD_HF_ST_048_Checkout_with_GuestUserCC_Engraving_and_Bundle.java
+++ src/test/java/TestExecute/Hydroflask/regressionTestcase/Test_DGLD_HF_ST_048_Checkout_with_GuestUserCC_Engraving_and_Bundle.java
@@ -5,14 +5,14 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import TestComponent.Hydroflask.GoldHydroHelper;
+import TestComponent.Hydroflask.GoldHydroHyvaHelper;
 import TestLib.Common;
 import TestLib.Login;
 
 public class Test_DGLD_HF_ST_048_Checkout_with_GuestUserCC_Engraving_and_Bundle {
 
 	String datafile = "Hydroflask//GoldHydroTestData.xlsx";
-	GoldHydroHelper Hydro = new GoldHydroHelper(datafile,"Engraving");
+	GoldHydroHyvaHelper Hydro = new GoldHydroHyvaHelper(datafile,"Engraving");
 
 	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
 	public void Validate_Checkout_with_GuestUserCC_Engraving_and_Bundle () throws Exception {
@@ -21,7 +21,7 @@ public class Test_DGLD_HF_ST_048_Checkout_with_GuestUserCC_Engraving_and_Bundle 
 			Hydro.verifingHomePage();
 			Hydro.search_product("Bundle product"); 
 			Hydro.Addtocart_Bundle("Bundle product");  
-			Hydro.search_product("Engraving Product");   
+			Hydro.bottles_headerlinks("Bottles & Drinkware");  
 			Hydro.Graphic_Engraving("Engraving Product");
 			Hydro.enraving_Checkout("Graphic");
 			Hydro.addDeliveryAddress_Guestuser("AccountDetails");
