1694acdf46a5be5381a4ccff02d6795df8f6a520
avayugundla <avayugundla@CPC-avayu-3AUJR.helenoftroy.com>
2024-11-12 21:18:09 +0530

    27 and 38 testcase for osprey UK

--- src/test/java/TestExecute/Osprey_EMEA/regressionTestcase/Test_DGLD_OS_COMMON_027_Guest_User_Checkout_Master_Card.java
+++ src/test/java/TestExecute/Osprey_EMEA/regressionTestcase/Test_DGLD_OS_COMMON_027_Guest_User_Checkout_Master_Card.java
@@ -5,14 +5,14 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import TestComponent.Osprey_EMEA.OspreyRegressionEMEA;
+import TestComponent.Osprey_EMEA.OspreyEMEA_HYVA;
 import TestLib.Common;
 import TestLib.Login;
 
 public class Test_DGLD_OS_COMMON_027_Guest_User_Checkout_Master_Card {
 
 	String datafile = "Osprey_EMEA//GoldOspreyemea.xlsx";
-	OspreyRegressionEMEA Osprey_ReEu = new OspreyRegressionEMEA(datafile,"Checkout payments");
+	OspreyEMEA_HYVA Osprey_ReEu = new OspreyEMEA_HYVA(datafile,"Checkout payments");
 
 	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
 	public void Verifying_Guest_User_Checkout_Master_Card () throws Exception {
