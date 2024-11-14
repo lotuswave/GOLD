b8223559b85c5df7a1b840b49efb523cdc0cd3d0
avayugundla <avayugundla@CPC-avayu-3AUJR.helenoftroy.com>
2024-11-14 17:55:14 +0530

    112 TESTCASE FOR OSPREY UK

--- src/test/java/TestExecute/Osprey_EMEA/regressionTestcase/TEST_DGLD_OS_COMMON_112_PRO_Customer_Checkout_with_Gift_card_Multiple_Line_Item_Using_CC.java
+++ src/test/java/TestExecute/Osprey_EMEA/regressionTestcase/TEST_DGLD_OS_COMMON_112_PRO_Customer_Checkout_with_Gift_card_Multiple_Line_Item_Using_CC.java
@@ -5,14 +5,14 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import TestComponent.Osprey_EMEA.OspreyRegressionEMEA;
+import TestComponent.Osprey_EMEA.OspreyEMEA_HYVA;
 import TestLib.Common;
 import TestLib.Login;
 
 public class TEST_DGLD_OS_COMMON_112_PRO_Customer_Checkout_with_Gift_card_Multiple_Line_Item_Using_CC {
 
 	String datafile = "Osprey_EMEA//GoldOspreyemea.xlsx";
-	OspreyRegressionEMEA Osprey_ReEu = new OspreyRegressionEMEA(datafile,"Giftcard Payments");
+	OspreyEMEA_HYVA Osprey_ReEu = new OspreyEMEA_HYVA(datafile,"Giftcard Payments");
 
 	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
 	public void Verifying_PRO_Customer_Checkout_with_Gift_card_Multiple_Line_Item_Using_CC () throws Exception {
@@ -23,14 +23,16 @@ public class TEST_DGLD_OS_COMMON_112_PRO_Customer_Checkout_with_Gift_card_Multip
         Osprey_ReEu.Login_Account("prouser");
         Osprey_ReEu.search_product("Product");
         Osprey_ReEu.addtocart("Product");
+        Osprey_ReEu.Prouser_Discount();
         Osprey_ReEu.search_product("Osprey Gift Card");
         Osprey_ReEu.Gift_cards("Osprey Gift Card");
         Osprey_ReEu.Card_Value("price");
         Osprey_ReEu.Bagpacks_headerlinks("Backpacks & Bags");
-        Osprey_ReEu.simple_addtocart("Simple product");  
+        Osprey_ReEu.simple_addtocart("Simple product"); 
+        Osprey_ReEu.Prouser_Discount();
         Osprey_ReEu.minicart_Checkout();
         Osprey_ReEu.RegaddDeliveryAddress("Account");
-        Osprey_ReEu.selectshippingmethod("ProShippingMethod");       //ProShippingMethod while executing us change this to proshipping method
+        Osprey_ReEu.selectshippingmethod("GroundShipping method");       
         Osprey_ReEu.clickSubmitbutton_Shippingpage();
         Osprey_ReEu.updatePaymentAndSubmitOrder("CCVisacard");
         
