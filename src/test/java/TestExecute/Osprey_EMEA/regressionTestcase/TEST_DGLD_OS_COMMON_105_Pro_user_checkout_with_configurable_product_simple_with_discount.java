5c82e367ed9260ed1052829dce62581b6139096e
avayugundla <avayugundla@CPC-avayu-3AUJR.helenoftroy.com>
2024-11-14 15:52:06 +0530

    105 testcase for osprey EMEA

--- src/test/java/TestExecute/Osprey_EMEA/regressionTestcase/TEST_DGLD_OS_COMMON_105_Pro_user_checkout_with_configurable_product_simple_with_discount.java
+++ src/test/java/TestExecute/Osprey_EMEA/regressionTestcase/TEST_DGLD_OS_COMMON_105_Pro_user_checkout_with_configurable_product_simple_with_discount.java
@@ -5,14 +5,14 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import TestComponent.Osprey_EMEA.OspreyRegressionEMEA;
+import TestComponent.Osprey_EMEA.OspreyEMEA_HYVA;
 import TestLib.Common;
 import TestLib.Login;
 
 public class TEST_DGLD_OS_COMMON_105_Pro_user_checkout_with_configurable_product_simple_with_discount {
 
 	String datafile = "Osprey_EMEA//GoldOspreyemea.xlsx";
-	OspreyRegressionEMEA Osprey_ReEu = new OspreyRegressionEMEA(datafile,"Checkout payments");
+	OspreyEMEA_HYVA Osprey_ReEu = new OspreyEMEA_HYVA(datafile,"Checkout payments");
 
 	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
 	public void Verifying_Pro_user_checkout_with_configurable_product_simple_with_discount () throws Exception {
@@ -23,11 +23,13 @@ public class TEST_DGLD_OS_COMMON_105_Pro_user_checkout_with_configurable_product
         Osprey_ReEu.Login_Account("prouser");
         Osprey_ReEu.search_product("Product");     
         Osprey_ReEu.addtocart("Product");
+        Osprey_ReEu.Prouser_Discount();
         Osprey_ReEu.Bagpacks_headerlinks("Backpacks & Bags");
-        Osprey_ReEu.simple_addtocart("Simple product");        
+        Osprey_ReEu.simple_addtocart("Simple product");
+        Osprey_ReEu.Prouser_Discount();
         Osprey_ReEu.minicart_Checkout();
         Osprey_ReEu.RegaddDeliveryAddress("Account");
-        Osprey_ReEu.selectshippingmethod("ProShippingMethod");    //ProShippingMethod while executing us change this to proshipping method
+        Osprey_ReEu.selectshippingmethod("GroundShipping method");    
         Osprey_ReEu.discountCode("Discount");
         Osprey_ReEu.clickSubmitbutton_Shippingpage();
         Osprey_ReEu.updatePaymentAndSubmitOrder("CCVisacard");
