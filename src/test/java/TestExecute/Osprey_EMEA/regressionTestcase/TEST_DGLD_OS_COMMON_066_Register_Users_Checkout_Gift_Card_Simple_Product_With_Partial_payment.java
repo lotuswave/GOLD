2dd78e95188bf142f5bd23c3fa243de3f148b31e
avayugundla <avayugundla@CPC-avayu-3AUJR.helenoftroy.com>
2024-11-15 14:23:16 +0530

    66 Test case for osprey UK

--- src/test/java/TestExecute/Osprey_EMEA/regressionTestcase/TEST_DGLD_OS_COMMON_066_Register_Users_Checkout_Gift_Card_Simple_Product_With_Partial_payment.java
+++ src/test/java/TestExecute/Osprey_EMEA/regressionTestcase/TEST_DGLD_OS_COMMON_066_Register_Users_Checkout_Gift_Card_Simple_Product_With_Partial_payment.java
@@ -5,14 +5,14 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import TestComponent.Osprey_EMEA.OspreyRegressionEMEA;
+import TestComponent.Osprey_EMEA.OspreyEMEA_HYVA;
 import TestLib.Common;
 import TestLib.Login;
 
 public class TEST_DGLD_OS_COMMON_066_Register_Users_Checkout_Gift_Card_Simple_Product_With_Partial_payment {
 
 	String datafile = "Osprey_EMEA//GoldOspreyemea.xlsx";
-	OspreyRegressionEMEA Osprey_ReEu = new OspreyRegressionEMEA(datafile,"Giftcard Payments");
+	OspreyEMEA_HYVA Osprey_ReEu = new OspreyEMEA_HYVA(datafile,"Giftcard Payments");
 
 	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
 	public void Verifying_Register_Users_Checkout_Gift_Card_Simple_Product_With_Partial_payment () throws Exception {
@@ -23,15 +23,12 @@ public class TEST_DGLD_OS_COMMON_066_Register_Users_Checkout_Gift_Card_Simple_Pr
         Osprey_ReEu.Login_Account("Account");
         Osprey_ReEu.search_product("Product");
         Osprey_ReEu.addtocart("Product");
-//        Osprey_ReEu.search_product("X-mas Gift Card");
-//        Osprey_ReEu.Gift_cards("X-mas Gift Card");
-//        Osprey_ReEu.Card_Value("price");
         Osprey_ReEu.minicart_Checkout();
         Osprey_ReEu.RegaddDeliveryAddress("Account");
         Osprey_ReEu.selectshippingmethod("GroundShipping method");
         Osprey_ReEu.clickSubmitbutton_Shippingpage();
         Osprey_ReEu.Gift_card("Partial Giftcard");
-        Osprey_ReEu.Partial_Payment("Partial Giftcard");
+        Osprey_ReEu.updatePaymentAndSubmitOrder("CCVisacard");
         
 		} catch (Exception e) {
 
