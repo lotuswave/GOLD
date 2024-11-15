8073b9366a2e1da99744e2bfc02c6ea062f753a5
avayugundla <avayugundla@CPC-avayu-3AUJR.helenoftroy.com>
2024-11-15 14:31:02 +0530

    94 testcase for osprey UK

--- src/test/java/TestExecute/Osprey_EMEA/regressionTestcase/Test_DGLD_OS_COMMON_094_Guest_UsersCheckout_Gift_Card_code_and_discount_With_Full_payment.java
+++ src/test/java/TestExecute/Osprey_EMEA/regressionTestcase/Test_DGLD_OS_COMMON_094_Guest_UsersCheckout_Gift_Card_code_and_discount_With_Full_payment.java
@@ -5,14 +5,14 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import TestComponent.Osprey_EMEA.OspreyRegressionEMEA;
+import TestComponent.Osprey_EMEA.OspreyEMEA_HYVA;
 import TestLib.Common;
 import TestLib.Login;
 
 public class Test_DGLD_OS_COMMON_094_Guest_UsersCheckout_Gift_Card_code_and_discount_With_Full_payment {
 
 	String datafile = "Osprey_EMEA//GoldOspreyemea.xlsx";
-	OspreyRegressionEMEA Osprey_ReEu = new OspreyRegressionEMEA(datafile,"Giftcard Payments");
+	OspreyEMEA_HYVA Osprey_ReEu = new OspreyEMEA_HYVA(datafile,"Giftcard Payments");
 
 	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
 	public void Verifying_Guest_user_Checkout_GiftCard_Code_and_Discount_With_FullPayment () throws Exception {
