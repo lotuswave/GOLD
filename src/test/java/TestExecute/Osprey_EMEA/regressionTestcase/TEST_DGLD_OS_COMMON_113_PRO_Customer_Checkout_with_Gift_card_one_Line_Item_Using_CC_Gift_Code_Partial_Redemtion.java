e3d899406695b39f3b127f9860dc4546d029d222
avayugundla <avayugundla@CPC-avayu-3AUJR.helenoftroy.com>
2024-11-15 14:17:44 +0530

    113 testcase for osprey UK

--- src/test/java/TestExecute/Osprey_EMEA/regressionTestcase/TEST_DGLD_OS_COMMON_113_PRO_Customer_Checkout_with_Gift_card_one_Line_Item_Using_CC_Gift_Code_Partial_Redemtion.java
+++ src/test/java/TestExecute/Osprey_EMEA/regressionTestcase/TEST_DGLD_OS_COMMON_113_PRO_Customer_Checkout_with_Gift_card_one_Line_Item_Using_CC_Gift_Code_Partial_Redemtion.java
@@ -5,14 +5,14 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import TestComponent.Osprey_EMEA.OspreyRegressionEMEA;
+import TestComponent.Osprey_EMEA.OspreyEMEA_HYVA;
 import TestLib.Common;
 import TestLib.Login;
 
 public class TEST_DGLD_OS_COMMON_113_PRO_Customer_Checkout_with_Gift_card_one_Line_Item_Using_CC_Gift_Code_Partial_Redemtion {
 
 	String datafile = "Osprey_EMEA//GoldOspreyemea.xlsx";
-	OspreyRegressionEMEA Osprey_ReEu = new OspreyRegressionEMEA(datafile,"Giftcard Payments");
+	OspreyEMEA_HYVA Osprey_ReEu = new OspreyEMEA_HYVA(datafile,"Giftcard Payments");
 
 	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
 	public void Verifying_PRO_Customer_Checkout_with_Gift_card_one_Line_Item_Using_CC_Gift_Code_Partial_Redemtion () throws Exception {
@@ -22,17 +22,15 @@ public class TEST_DGLD_OS_COMMON_113_PRO_Customer_Checkout_with_Gift_card_one_Li
         Osprey_ReEu.click_singinButton();
         Osprey_ReEu.Login_Account("prouser");
         Osprey_ReEu.search_product("Product");
-        Osprey_ReEu.addtocart("Product");
-//        Osprey_ReEu.search_product("X-mas Gift Card");
-//        Osprey_ReEu.Gift_cards("X-mas Gift Card");
-//        Osprey_ReEu.Card_Value("price");  
+        Osprey_ReEu.addtocart("Product"); 
+        Osprey_ReEu.Prouser_Discount();
         Osprey_ReEu.minicart_Checkout();
         Osprey_ReEu.RegaddDeliveryAddress("Account");
-        Osprey_ReEu.selectshippingmethod("ProShippingMethod");      //ProShippingMethod while executing us change this to proshipping method
+        Osprey_ReEu.selectshippingmethod("GroundShipping method");      
         Osprey_ReEu.clickSubmitbutton_Shippingpage();
         Osprey_ReEu.Gift_card("Partial Giftcard");
         Osprey_ReEu.updatePaymentAndSubmitOrder("CCVisacard");
-//        Osprey_ReEu.Partial_Payment("Partial Giftcard");
+
         
 		} catch (Exception e) {
 
