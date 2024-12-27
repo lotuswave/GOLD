44de4ea87e4ea875679ea728478a07ce91d3cbce
avayugundla <avayugundla@CPC-avayu-3AUJR.helenoftroy.com>
2024-12-27 16:55:26 +0530

    7 testcase name changes for E2E oxo us

--- src/test/java/TestExecute/OXO/OXO_O2C_E2E/Test_DGLD_OXO_E2E_ST_007_GuestUser_checkout_with_CC_2Day_with_Multipleitems_BundleItem_MultipleQty_Giftmessage.java
+++ src/test/java/TestExecute/OXO/OXO_O2C_E2E/Test_DGLD_OXO_E2E_ST_007_GuestUser_checkout_with_CC_2Day_with_Multipleitems_BundleItem_MultipleQty_Giftmessage.java
@@ -7,20 +7,17 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import TestComponent.OXO.GoldOxoHelper;
-import TestLib.Login;
+import TestComponent.OXO.GoldOxoE2EHelper;
+import TestLib.*;
 
-public class Test_DGLD_OXO_E2E_ST_007_Ecom_GuestUser_Afterpay_2_Items_Standard_Shipping {
+public class Test_DGLD_OXO_E2E_ST_007_GuestUser_checkout_with_CC_2Day_with_Multipleitems_BundleItem_MultipleQty_Giftmessage {
 
 	String datafile = "OXO//GoldOxoTestData.xlsx";	
-	GoldOxoHelper Oxo=new GoldOxoHelper(datafile,"E2E");
+	GoldOxoE2EHelper Oxo=new GoldOxoE2EHelper(datafile,"E2E");
 	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
-	public void Validate_GuestUser_Afterpay_2_Items_Standard_Shipping () throws Exception {
-//       for(int i=0;i<3;i++)
-//       {
+	public void Validate_GuestUser_checkout_with_CC_2Day_with_Multipleitems_BundleItem_MultipleQty_Giftmessage () throws Exception {
 		try {
 			Oxo.prepareOrdersData("OXO_E2E_orderDetails.xlsx");
-			Thread.sleep(5000);
 			String Description ="GuestUser_Afterpay_2_Items_Standard_Shipping ";
 			Oxo.verifingHomePage();
 			Oxo.search_E2E_product("SKU-11262700");
@@ -33,17 +30,15 @@ public class Test_DGLD_OXO_E2E_ST_007_Ecom_GuestUser_Afterpay_2_Items_Standard_S
 			Oxo.select_Shipping_Method("GroundShipping method");
 			Oxo.clickSubmitbutton_Shippingpage();
 			HashMap<String,String> data=Oxo.OrderSummaryValidation();
-			HashMap<String,String> AfterPayment=Oxo.AfterPayment("Afterpay");
+			String Used_GiftCode="Null";
+			String OrderNumber=Oxo.updatePaymentAndSubmitOrder("PaymentDetails"); 
 			String OrderIdNumber= Oxo.Verify_order_page();
 			System.out.println(OrderIdNumber);
-			Oxo.Admin("Login Details");
+			Oxo.Admin_signin("Login Details");
 			Oxo.click_Sales();
-			HashMap<String,String> Orderstatus1 = Oxo.Admin_Order_Details(OrderIdNumber);
-			Oxo.writeOrderNumber(OrderIdNumber, Description, data.get("subtotlaValue"),data.get("shippingammountvalue"),data.get("Taxammountvalue"),data.get("ActualTotalammountvalue"),data.get("ExpectedTotalAmmountvalue"),data.get("Discountammountvalue"),Shipping.get("ShippingState"),Shipping.get("ShippingZip"),AfterPayment.get("Card"),Products_details,Orderstatus1.get("AdminOrderstatus"),Orderstatus1.get("AdminOrdertax"),Orderstatus1.get("AdminOrdertotal"));
-
-			
-			
-			
+			HashMap<String,String> Orderstatus1 = Oxo.Admin_Order_Details(OrderNumber);
+			Oxo.writeOrderNumber(Description,OrderNumber,Orderstatus1.get("Skus"),Orderstatus1.get("AdminOrderstatus"),Used_GiftCode);
+	
 
 		} catch (Exception e) {
 
@@ -54,7 +49,7 @@ public class Test_DGLD_OXO_E2E_ST_007_Ecom_GuestUser_Afterpay_2_Items_Standard_S
 	
 	@AfterTest
 	public void clearBrowser() {
-		//Common.closeAll();
+		Common.closeAll();
 
 	}
 
