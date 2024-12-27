4ba5743c16053e60be0472fccca0da1e99e54c11
avayugundla <avayugundla@CPC-avayu-3AUJR.helenoftroy.com>
2024-12-27 18:16:58 +0530

    7 testcase for oxo us E2E

--- src/test/java/TestExecute/OXO/OXO_O2C_E2E/Test_DGLD_OXO_E2E_ST_007_GuestUser_checkout_with_CC_2Day_with_Multipleitems_BundleItem_MultipleQty_Giftmessage.java
+++ src/test/java/TestExecute/OXO/OXO_O2C_E2E/Test_DGLD_OXO_E2E_ST_007_GuestUser_checkout_with_CC_2Day_with_Multipleitems_BundleItem_MultipleQty_Giftmessage.java
@@ -18,22 +18,21 @@ public class Test_DGLD_OXO_E2E_ST_007_GuestUser_checkout_with_CC_2Day_with_Multi
 	public void Validate_GuestUser_checkout_with_CC_2Day_with_Multipleitems_BundleItem_MultipleQty_Giftmessage () throws Exception {
 		try {
 			Oxo.prepareOrdersData("OXO_E2E_orderDetails.xlsx");
-			String Description ="GuestUser_Afterpay_2_Items_Standard_Shipping ";
+			String Description ="Guest user checkout with CreditCard-Visa  (2 Day) - Multiple items + Bundle Item + Multiple Qty +  Gift message(greater than 50 charecters)";
 			Oxo.verifingHomePage();
-			Oxo.search_E2E_product("SKU-11262700");
-			Oxo.Addtocart("SKU-11262700");
-			Oxo.search_E2E_product("SKU-11404000");
-			Oxo.Addtocart("SKU-11404000");
-		    Oxo.minicart_Checkout();
-			String Products_details=Oxo.shipping_order_details();
-			HashMap<String,String> Shipping=Oxo.Shipingdetails("AccountDetails");
-			Oxo.select_Shipping_Method("GroundShipping method");
-			Oxo.clickSubmitbutton_Shippingpage();
-			HashMap<String,String> data=Oxo.OrderSummaryValidation();
-			String Used_GiftCode="Null";
+			Oxo.search_product("SKU-11261400 - 2QTY");
+			Oxo.addtocart("SKU-11261400 - 2QTY");
+			Oxo.search_product("SKU-32480 -2QTY");
+			Oxo.addtocart("SKU-32480 -2QTY");
+			Oxo.search_product("Bundle Product");
+			Oxo.addtocart("Bundle Product");
+			Oxo.minicart_viewcart();
+			Oxo.Gift_Message("Giftmessage50letters");
+			Oxo.minicart_Checkout();
+			Oxo.addDeliveryAddress_Guest("AccountDetails");
+		    Oxo.select_Shipping_Method("2 Day method");
+			String Used_GiftCode="NULL";
 			String OrderNumber=Oxo.updatePaymentAndSubmitOrder("PaymentDetails"); 
-			String OrderIdNumber= Oxo.Verify_order_page();
-			System.out.println(OrderIdNumber);
 			Oxo.Admin_signin("Login Details");
 			Oxo.click_Sales();
 			HashMap<String,String> Orderstatus1 = Oxo.Admin_Order_Details(OrderNumber);
