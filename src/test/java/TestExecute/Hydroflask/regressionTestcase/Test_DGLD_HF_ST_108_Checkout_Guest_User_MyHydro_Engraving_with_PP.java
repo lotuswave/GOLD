80567a7dcd2d715d04f05000ad330fd2d22168f1
avayugundla <avayugundla@CPC-avayu-3AUJR.helenoftroy.com>
2025-01-13 18:27:28 +0530

    108 and 109 testcase for hydroflask us

--- src/test/java/TestExecute/Hydroflask/regressionTestcase/Test_DGLD_HF_ST_108_Checkout_Guest_User_MyHydro_Engraving_with_PP.java
+++ src/test/java/TestExecute/Hydroflask/regressionTestcase/Test_DGLD_HF_ST_108_Checkout_Guest_User_MyHydro_Engraving_with_PP.java
@@ -5,14 +5,14 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import TestComponent.Hydroflask.GoldHydroHelper;
+import TestComponent.Hydroflask.GoldHydroHyvaHelper;
 import TestLib.Common;
 import TestLib.Login;
 
 public class Test_DGLD_HF_ST_108_Checkout_Guest_User_MyHydro_Engraving_with_PP {
 
 	String datafile = "Hydroflask//GoldHydroTestData.xlsx";
-	GoldHydroHelper Hydro = new GoldHydroHelper(datafile,"Engraving");
+	GoldHydroHyvaHelper Hydro = new GoldHydroHyvaHelper(datafile,"Engraving");
 
 	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
 	public void Validate_Checkout_Guest_User_MyHydro_Engraving_with_PP () throws Exception {
