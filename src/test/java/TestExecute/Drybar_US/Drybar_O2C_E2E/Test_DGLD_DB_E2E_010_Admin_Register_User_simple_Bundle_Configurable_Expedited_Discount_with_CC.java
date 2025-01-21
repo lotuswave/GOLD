a1c22e30d0cc8d20b0266c627b5b10e499a335e2
avayugundla <avayugundla@CPC-avayu-3AUJR.helenoftroy.com>
2025-01-21 12:12:32 +0530

    Admin 1 testcase for drybar us

---
+++ src/test/java/TestExecute/Drybar_US/Drybar_O2C_E2E/Test_DGLD_DB_E2E_010_Admin_Register_User_simple_Bundle_Configurable_Expedited_Discount_with_CC.java
@@ -0,0 +1,66 @@
+package TestExecute.Drybar_US.Drybar_O2C_E2E;
+
+import java.util.HashMap;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import TestComponent.Drybar_US.GoldDrybarusE2EHelper;
+import TestLib.Common;
+import TestLib.Login;
+
+public class Test_DGLD_DB_E2E_010_Admin_Register_User_simple_Bundle_Configurable_Expedited_Discount_with_CC {
+
+	String datafile = "Drybar_US//GoldDrybarTestData.xlsx";
+	GoldDrybarusE2EHelper Drybar = new GoldDrybarusE2EHelper(datafile,"Drybar_E2E");;
+
+	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
+	public void Verifying_Admin_Register_User_simple_Bundle_Configurable_Expedited_Discount_with_CC () throws Exception {
+
+		try {
+		Drybar.prepareOrdersData("Drybar_E2E_orderDetails.xlsx");
+		String Description="Place order on Admin- Add Simple + Bundle item + Configurable  item + Expedited shipping + Discount + CC";
+        String Used_GiftCode = "NULL"; 
+        Drybar.Admin_signin("AccountDetails");
+        Drybar.Customers();
+        Drybar.Allcustomers();
+        Drybar.SelectCustomer_Edit("DB_Store");
+        Drybar.Click_CreatNewOrders();
+        Drybar.Select_store("DB_Store");
+        Drybar.Add_product_SKU("DB_Store");
+        Drybar.bundle_Item();
+        Drybar.Admin_Discount("Discount");
+        Drybar.Guestuser_shippingaddress("AccountDetails");
+        Drybar.Select_Shipping_method("AccountDetails");
+        String OrderNumber= Drybar.Default_Payment_method("CCAmexcard");
+        Drybar.click_Sales();
+        HashMap<String, String> Orderstatus1= Drybar.order_verfication(OrderNumber);
+        Drybar.writeOrderNumber(Description,OrderNumber,Orderstatus1.get("Skus"),Orderstatus1.get("AdminOrderstatus"),Orderstatus1.get("workato"),Used_GiftCode);
+        
+		} catch (Exception e) {
+
+			Assert.fail(e.getMessage(), e);
+		}
+	}
+
+
+	@AfterTest
+	public void clearBrowser() {
+		Common.closeAll();
+
+	}
+
+	@BeforeTest
+	public void startTest() throws Exception {
+		System.setProperty("configFile", "Drybar_US\\config.properties");
+        Login.signIn();
+        Drybar.close_add();
+        
+
+	}
+
+}
+
+
