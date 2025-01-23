2d5d413a85a3e109c10a3cde33954030aedf8f63
vnarra <vnarra@CPC-vnarr-42JDJ.helenoftroy.com>
2025-01-23 16:54:08 +0530

    53 Testcase OXO

---
+++ src/test/java/TestExecute/OXO/SmokeTestCases/Test_DGLD_OXO_ST_053_Validate_SearchResults_Page_PLP_and_PDP_page_Regular_Item_GC_Purchase.java
@@ -0,0 +1,10 @@
+package TestExecute.OXO.SmokeTestCases;
+
+public class Test_DGLD_OXO_ST_053_Validate_SearchResults_Page_PLP_and_PDP_page_Regular_Item_GC_Purchase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
