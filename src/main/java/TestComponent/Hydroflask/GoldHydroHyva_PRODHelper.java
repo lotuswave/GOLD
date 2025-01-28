a17313fa5f2861c130ac75db6f417022f5d0eab2
avayugundla <avayugundla@CPC-avayu-3AUJR.helenoftroy.com>
2025-01-28 12:42:02 +0530

    53 prod smoke testcase for hydroflask us

--- src/main/java/TestComponent/Hydroflask/GoldHydroHyva_PRODHelper.java
+++ src/main/java/TestComponent/Hydroflask/GoldHydroHyva_PRODHelper.java
@@ -173,6 +173,46 @@ public class GoldHydroHyva_PRODHelper {
 
 	}
 	
+	public void Accessories_headerlinks(String category) {
+		// TODO Auto-generated method stub
+		String expectedResult = "User should click the" + category;
+		try {
+
+			Sync.waitElementPresent("xpath", "(//a[contains(@title,'Shop')]//span[contains(text(),'Shop')])[1]");
+			Thread.sleep(3000);
+//			Common.scrollIntoView("xpath","//a[contains(@class,'level-top')]//span[text()=' Shop']");
+			Common.clickElement("xpath", "(//a[contains(@title,'Shop')]//span[contains(text(),'Shop')])[1]");
+
+			Thread.sleep(3000);
+
+			try {
+				Common.mouseOver("xpath", "//span[contains(text(),'" + category + "')]");
+			} catch (Exception e) {
+				Common.clickElement("xpath", "//a[@class='level-top ui-corner-all']//span[text()=' Shop']");
+			}
+			Common.clickElement("xpath", "//span[contains(text(),'" + category + "')]");
+			Thread.sleep(4000);
+			Common.clickElement("xpath", "//a[contains(@href,'bottles-drinkware')]//span[text()='Accessories']");
+			Sync.waitPageLoad();
+			Thread.sleep(6000);
+			expectedResult = "User should select the " + category + "category";
+			int sizebotteles = Common.findElements("xpath", "//span[contains(text(),'" + category + "')]").size();
+			Common.assertionCheckwithReport(sizebotteles > 0,
+					"validating the product category as" + category + "from navigation menu ", expectedResult,
+					"Selected the " + category + " category", "User unabel to click" + category + "");
+
+		}
+
+		catch (Exception | Error e) {
+			e.printStackTrace();
+			ExtenantReportUtils.addFailedLog("validating the product category as" + category + "from navigation menu ",
+					expectedResult, "Unable to Selected the " + category + " category",
+					Common.getscreenShot("Failed to click on the" + category + ""));
+
+			Assert.fail();
+		}
+
+	}
 	
 	
 	public void Bottles_headerlinks(String category) {
---
+++ src/test/java/TestExecute/Hydroflask/SmokeTestCases/Test_DGLD_HYF_ST_053_Validate_SearchResults_Page_PLP_and_PDP_page_Regular_Item_GC_Purchase.java
@@ -0,0 +1,65 @@
+package TestExecute.Hydroflask.SmokeTestCases;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import TestComponent.Hydroflask.GoldHydroHyva_PRODHelper;
+import TestLib.Common;
+import TestLib.Login;
+
+public class Test_DGLD_HYF_ST_053_Validate_SearchResults_Page_PLP_and_PDP_page_Regular_Item_GC_Purchase {
+
+		String datafile = "Hydroflask//GoldHydroTestData.xlsx";
+		GoldHydroHyva_PRODHelper Hydro = new GoldHydroHyva_PRODHelper(datafile,"DataSet");
+		GoldHydroHyva_PRODHelper Hydro1 = new GoldHydroHyva_PRODHelper(datafile, "PDP");
+
+		@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
+		public void Validate_SearchResults_Page_PLP_and_PDP_page_Regular_Item_GC_Purchase () throws Exception {
+
+			try {
+				Hydro.verifingHomePage();
+				Hydro.Validating_search("aaabbcc");
+				Hydro.search_results("aaabbcc");
+				Hydro.popular_searches();
+				//Hydro.carousel();
+				Hydro.search_product("Product");
+				Hydro.Accessories_headerlinks("Bottles & Drinkware"); 
+				Hydro.view_PLP_page();
+				Hydro.sort_By("SortBy");
+				Hydro.filter_By("Collections");
+	//			Hydro.color_validation("Black");
+				Hydro.price_filter_validation();
+				Hydro1.search_product("Product"); 	
+				Hydro1.Configurableproduct_addtocart_pdppage("Product");
+				Hydro1.Configurableproduct_addtocart_pdp("Product");
+				Hydro1.writeareview("Product");
+				Hydro.Gift_cards("Hydro Gift Card");
+				Hydro.Card_Value("price");
+				Hydro.minicart_Checkout();
+				Hydro.addDeliveryAddress_Guestuser("AccountDetails");
+				Hydro.updatePaymentAndSubmitOrder("PaymentDetails");
+						
+
+			} catch (Exception e) {
+
+				Assert.fail(e.getMessage(), e);
+			}
+		}
+
+		@AfterTest
+		public void clearBrowser() {
+			Common.closeAll();
+
+		}
+
+		@BeforeTest
+		public void startTest() throws Exception {
+			System.setProperty("configFile", "Hydroflask\\config.properties");
+			Login.signIn();
+			Hydro.close_add();
+	        Hydro.acceptPrivacy();
+		}
+
+	}
