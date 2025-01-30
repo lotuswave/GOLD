369ac7b91edae90eef4fdeb07ebacf9d10755e94
vnarra <vnarra@CPC-vnarr-42JDJ.helenoftroy.com>
2025-01-30 15:02:33 +0530

    Osprey-EU:(Smoke) TC-054 & TC-008 Helper changed to Prod Helper

--- src/test/java/TestExecute/Osprey_US/SmokeTestCases/Test_DGLD_OSP_US_BCST_005_HeaderLinks.java
+++ src/test/java/TestExecute/Osprey_US/SmokeTestCases/Test_DGLD_OSP_US_BCST_005_HeaderLinks.java
@@ -7,42 +7,41 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import TestComponent.Osprey_US.GoldOspreyUSHyvaHelper;
+import TestComponent.Osprey_US.GoldOspreyUS_PRODHyvaHelper;
 import TestLib.Common;
 import TestLib.Login;
 
 public class Test_DGLD_OSP_US_BCST_005_HeaderLinks {
 
 	String datafile = "Osprey_US//GoldOspreyus.xlsx";
-	GoldOspreyUSHyvaHelper Osprey_ReEu = new GoldOspreyUSHyvaHelper(datafile,"Header");
+	GoldOspreyUS_PRODHyvaHelper Osprey_ReEu = new GoldOspreyUS_PRODHyvaHelper(datafile, "Header");
 
 	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
-	public void Verifying_Header_Links_Functionality () throws Exception {
+	public void Verifying_Header_Links_Functionality() throws Exception {
 
 		try {
 			Osprey_ReEu.verifingHomePage();
 			Osprey_ReEu.header_OutdoorPacks("Outdoor Packs");
 			Osprey_ReEu.header_KidsPacks("Kids Packs Carriers");
-			Osprey_ReEu.header_ChildCarrier("Child Carriers");  
+			Osprey_ReEu.header_ChildCarrier("Child Carriers");
 			Osprey_ReEu.header_DayPacks("Day Packs");
 			Osprey_ReEu.header_Travel("Travel");
 			Osprey_ReEu.header_Luggage("Travel");
-			Osprey_ReEu.header_Accessories("Accessories");         
-			Osprey_ReEu.header_Featured("Featured");  
-			Osprey_ReEu.header_Holiday_Gift_Guide("Featured");      //no need include in prod
-			Osprey_ReEu.Bagpack_ShopAll("ShopAll");   
-			Osprey_ReEu.Travel_ShopAll("TravelShopAll");                 
-			Osprey_ReEu.header_Shopbyactivity("Shop by Activity");       
-			Osprey_ReEu.header_Shopbycollection("Shop by Collections"); 
-			Osprey_ReEu.Featured_ShopAll("FeaturedShopAll");      
-			Osprey_ReEu.header_Explore("Explore"); 
-        
-        
+			Osprey_ReEu.header_Accessories("Accessories");
+			Osprey_ReEu.header_Featured("Featured");
+			Osprey_ReEu.header_Holiday_Gift_Guide("Featured"); // no need include in prod
+			Osprey_ReEu.Bagpack_ShopAll("ShopAll");
+			Osprey_ReEu.Travel_ShopAll("TravelShopAll");
+			Osprey_ReEu.header_Shopbyactivity("Shop by Activity");
+			Osprey_ReEu.header_Shopbycollection("Shop by Collections");
+			Osprey_ReEu.Featured_ShopAll("FeaturedShopAll");
+			Osprey_ReEu.header_Explore("Explore");
+
 		} catch (Exception e) {
 			Assert.fail(e.getMessage(), e);
 		}
 	}
 
-	
 	@AfterTest
 	public void clearBrowser() {
 		Common.closeAll();
@@ -52,8 +51,8 @@ public class Test_DGLD_OSP_US_BCST_005_HeaderLinks {
 	@BeforeTest
 	public void startTest() throws Exception {
 		System.setProperty("configFile", "Osprey_US\\config.properties");
-        Login.signIn();
-        
+		Login.signIn();
+
 	}
 
 }
