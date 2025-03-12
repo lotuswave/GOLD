35ad1e6d40bfd14f00bb3910da14a1d5b17593f8
avayugundla <avayugundla@CPC-avayu-3AUJR.helenoftroy.com>
2025-03-12 13:00:18 +0530

    5 th testcase for Hydroflask US

--- src/test/java/TestExecute/Hydroflask/regressionTestcase/TEST_DGLD_HYF_US_ST_005_Guest_User_Checkout_with_Gift_Card_Code.java
+++ src/test/java/TestExecute/Hydroflask/regressionTestcase/TEST_DGLD_HYF_US_ST_005_Guest_User_Checkout_with_Gift_Card_Code.java
@@ -9,24 +9,21 @@ import TestComponent.Hydroflask.GoldHydroHyvaHelper;
 import TestLib.Common;
 import TestLib.Login;
 
-public class Test_DGLD_HYF_US_ST_006_GiftCard_And_ConfigurableProduct_Checkout_With_PromoCode_Applied {
-
+public class TEST_DGLD_HYF_US_ST_005_Guest_User_Checkout_with_Gift_Card_Code {
 	String datafile = "Hydroflask//GoldHydroTestData.xlsx";
 	GoldHydroHyvaHelper Hydro = new GoldHydroHyvaHelper(datafile,"DataSet");
 	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
-	public void Verifying_GuestUser_GiftCard_And_ConfigurableProduct_Checkout_With_PromoCode_CC () throws Exception {
+	public void Verifying_Guest_User_Checkout_with_Gift_Card_Code () throws Exception {
 
 		try {
         Hydro.verifingHomePage();
-		Hydro.bottles_headerlinks("Bottles & Drinkware"); 
-		Hydro.Configurable_addtocart_pdp("Product");
-		Hydro.Gift_cards("Hydro Gift Card");
-		Hydro.Card_Value("price");
+		Hydro.search_product("Product");     
+		Hydro.addtocart("Product");
 		Hydro.minicart_Checkout();
 		Hydro.addDeliveryAddress_Guestuser("AccountDetails");
         Hydro.selectshippingaddress("GroundShipping method");
         Hydro.clickSubmitbutton_Shippingpage();
-        Hydro.discountCode("Discount");
+        Hydro.Gift_card("Giftcard");
         Hydro.updatePaymentAndSubmitOrder("PaymentDetails");
         
 		} catch (Exception e) {
