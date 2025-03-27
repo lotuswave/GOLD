481e43a5334670aa3c839b36b0a16b6960756a75
avayugundla <avayugundla@CPC-avayu-3AUJR.helenoftroy.com>
2025-03-27 13:19:42 +0530

    Test_DGLD_OS_US_ST_074_Guest_user_Checkout_With_Discount_Amex_card
    helper name changes for this testcase

--- src/test/java/TestExecute/Osprey_US/RegressionTestcase/Test_DGLD_OS_US_ST_074_Guest_user_Checkout_With_Discount_Amex_card.java
+++ src/test/java/TestExecute/Osprey_US/RegressionTestcase/Test_DGLD_OS_US_ST_074_Guest_user_Checkout_With_Discount_Amex_card.java
@@ -5,14 +5,14 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import TestComponent.Osprey_EMEA.OspreyRegressionEMEA;
+import TestComponent.Osprey_US.GoldOspreyUSHyvaHelper;
 import TestLib.Common;
 import TestLib.Login;
 
 public class Test_DGLD_OS_US_ST_074_Guest_user_Checkout_With_Discount_Amex_card {
 
 	String datafile = "Osprey_EMEA//GoldOspreyemea.xlsx";
-	OspreyRegressionEMEA Osprey_ReEu = new OspreyRegressionEMEA(datafile,"Checkout payments");
+	GoldOspreyUSHyvaHelper Osprey_ReEu = new GoldOspreyUSHyvaHelper(datafile,"Checkout payments");
 
 	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
 	public void Verifying_Guest_user_Checkout_With_Discount_Amex_card () throws Exception {
