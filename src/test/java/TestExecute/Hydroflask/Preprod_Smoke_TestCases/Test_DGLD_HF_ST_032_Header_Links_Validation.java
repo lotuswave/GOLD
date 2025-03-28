cffd94f081bb1301eaf6e300447dacd96452dc17
spanem <spanem@CPC-spane-J7PSO.helenoftroy.com>
2025-03-28 19:30:33 +0530

    HYF - DQA-165 test case updated

--- src/test/java/TestExecute/Hydroflask/Preprod_Smoke_TestCases/Test_DGLD_HF_ST_032_Header_Links_Validation.java
+++ src/test/java/TestExecute/Hydroflask/Preprod_Smoke_TestCases/Test_DGLD_HF_ST_032_Header_Links_Validation.java
@@ -25,12 +25,12 @@ public class Test_DGLD_HF_ST_032_Header_Links_Validation {
         	Hydro.Coolers_validation("Coolers");  
          	Hydro.kitchenware_validation("kitchenware");
       		Hydro.Accessories_validation("Accessories");
-            Hydro.featured_validation("Featured"); 
+     
             Hydro.Collections_validation("Collections");
             Hydro.New_Color_Destination("Colors");
 			Hydro.Explore_Validation("Explore Links");
 			Hydro.Customize_validation("Customize");
-			
+//		       Hydro.featured_validation("Featured"); 
 			
 			//         Hydro.Holiday_shop_validation("Holiday Sale"); 
 			
@@ -43,7 +43,7 @@ public class Test_DGLD_HF_ST_032_Header_Links_Validation {
 
 	@AfterTest
 	public void clearBrowser() {
-//		Common.closeAll();
+		Common.closeAll();
 
 	}
 
