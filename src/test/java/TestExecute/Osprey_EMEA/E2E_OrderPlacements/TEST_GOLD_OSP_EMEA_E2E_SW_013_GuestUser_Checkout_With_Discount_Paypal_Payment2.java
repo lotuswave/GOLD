c143a778ebb26d1963b4d4f9614fdd2fd3b9abad
avayugundla <avayugundla@CPC-avayu-3AUJR.helenoftroy.com>
2025-03-29 12:25:08 +0530

    E2E changes osprey emea

--- src/test/java/TestExecute/Osprey_EMEA/E2E_OrderPlacements/TEST_GOLD_OSP_EMEA_E2E_SW_013_GuestUser_Checkout_With_Discount_Paypal_Payment2.java
+++ src/test/java/TestExecute/Osprey_EMEA/E2E_OrderPlacements/TEST_GOLD_OSP_EMEA_E2E_SW_013_GuestUser_Checkout_With_Discount_Paypal_Payment2.java
@@ -46,8 +46,6 @@ public class TEST_GOLD_OSP_EMEA_E2E_SW_013_GuestUser_Checkout_With_Discount_Payp
 			Assert.fail(e.getMessage(), e);
 		}
 	}
-
-
 	@AfterTest
 	public void clearBrowser() {
 		Common.closeAll();
