c1eb9197b1fa71109eb1f701156b84bd7563084c
anbogi <anbogi@CPC-anbog-RVKLH.helenoftroy.com>
2025-03-29 21:08:13 +0530

    Osprey_us:Test_DGLD_OS_004 changes

--- src/test/java/TestExecute/Osprey_US/RegressionTestcase/Test_DGLD_OS_004_PlaceOrder_ValidateShipping_Methods_For_NonPro_Employee_Non_ContinentalUS_SubtotalAmount_Lessthan_OR_Equal_49StandardShipping5.java
+++ src/test/java/TestExecute/Osprey_US/RegressionTestcase/Test_DGLD_OS_004_PlaceOrder_ValidateShipping_Methods_For_NonPro_Employee_Non_ContinentalUS_SubtotalAmount_Lessthan_OR_Equal_49StandardShipping5.java
@@ -15,18 +15,17 @@ public class Test_DGLD_OS_004_PlaceOrder_ValidateShipping_Methods_For_NonPro_Emp
 	GoldOspreyUSHyvaHelper Osprey_ReEu = new GoldOspreyUSHyvaHelper(datafile,"Checkout payments");
 
 	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
-	public void Guest_user_checkout_PP_with_Multiple_products_with_Discount_samebilling_and_shipping () throws Exception {
+	public void Test_DGLD_OS_004_PlaceOrder_ValidateShipping_Methods_For_NonPro_Employee_Non_ContinentalUS_SubtotalAmount_Lessthan_OR_Equal_49StandardShipping5 () throws Exception {
    
 		try {
 			 Osprey_ReEu.verifingHomePage();
 		        Osprey_ReEu.search_product("Product<50");
 		        Osprey_ReEu.simple_addtocart("Product<50");
-		        Osprey_ReEu.minicart_Checkout();
-		       
+		        Osprey_ReEu.minicart_Checkout();		       
 		        Osprey_ReEu.addDeliveryAddress_Guestuser("Non_Continental");
 		        Osprey_ReEu.selectshippingmethod("GroundShipping method");
 		        Osprey_ReEu.Verify_ShippingAmount_Lessthan_Or_Equal49();
-		        Osprey_ReEu.clickSubmitbutton_Shippingpage();
+		       // Osprey_ReEu.clickSubmitbutton_Shippingpage();
 		        Osprey_ReEu.updatePaymentAndSubmitOrder("CCVisacard");
 		        
         
