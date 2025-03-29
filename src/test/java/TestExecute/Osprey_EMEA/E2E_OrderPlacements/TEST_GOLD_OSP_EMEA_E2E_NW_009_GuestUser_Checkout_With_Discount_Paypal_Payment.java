3fd49990268b70672fffbaf50399f2896fb0b1b8
avayugundla <avayugundla@CPC-avayu-3AUJR.helenoftroy.com>
2025-03-29 12:09:04 +0530

    TEST_GOLD_OSP_EMEA_E2E_NW_009_GuestUser_Checkout_With_Discount_Paypal_Payment
    gift card code changes

--- src/test/java/TestExecute/Osprey_EMEA/E2E_OrderPlacements/TEST_GOLD_OSP_EMEA_E2E_NW_009_GuestUser_Checkout_With_Discount_Paypal_Payment.java
+++ src/test/java/TestExecute/Osprey_EMEA/E2E_OrderPlacements/TEST_GOLD_OSP_EMEA_E2E_NW_009_GuestUser_Checkout_With_Discount_Paypal_Payment.java
@@ -23,7 +23,7 @@ public class TEST_GOLD_OSP_EMEA_E2E_NW_009_GuestUser_Checkout_With_Discount_Payp
 			
 	     	Osprey_ReEu.prepareOrdersData("OspreyEU_E2E_orderDetails.xlsx");
 			String Description = "Norway Store  -Discount + Paypal";
-			Osprey_ReEu.verifingHomePage();
+//			Osprey_ReEu.verifingHomePage();
 			Osprey_ReEu.search_product("SKU-10002926 -3QTY");
 			Osprey_ReEu.simple_addtocart("SKU-10002926 -3QTY");
 			Osprey_ReEu.minicart_Checkout();
@@ -33,7 +33,7 @@ public class TEST_GOLD_OSP_EMEA_E2E_NW_009_GuestUser_Checkout_With_Discount_Payp
 			Osprey_ReEu.discountCode("Discount");
 //			Osprey_ReEu.clickSubmitbutton_Shippingpage();
 			  HashMap<String,String> Details=Osprey_ReEu.ordersummary_Details();
-			String OrderNumber =Osprey_ReEu.payPal_Payment("PaypalDetails");
+			String OrderNumber =Osprey_ReEu.payPal_Payment("NON_UK_PaypalDetails");
 //			Osprey_ReEu.Admin_signin("Login Details");
 //		    Osprey_ReEu.click_Sales();
 //			HashMap<String,String> Orderstatus1 = Osprey_ReEu.Admin_Order_Details(OrderNumber);
