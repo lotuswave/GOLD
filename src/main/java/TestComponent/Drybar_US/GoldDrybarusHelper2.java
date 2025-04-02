513d381e850cd26cef792022e3fcf6bc2c67b162
mmarella <mmarella@CPC-mmare-M3HIN.helenoftroy.com>
2025-04-02 23:10:40 +0530

    DQA-3660-FullGC_validationupdate

--- src/main/java/TestComponent/Drybar_US/GoldDrybarusHelper2.java
+++ src/main/java/TestComponent/Drybar_US/GoldDrybarusHelper2.java
@@ -4966,8 +4966,11 @@ public void FUll_Payment(String dataSet) {
 	//Common.clickElement("xpath", "//button[@class='action primary checkout']");
 	//Thread.sleep(4000);
 	//order = Common.getText("xpath", "//div[contains(@class,'checkout-success')]//p//span");
-         	    order = Common.getText("xpath", "//div[contains(@class,'checkout-success')]//p//a");
+         	    order = Common.getText("xpath", "(//div[contains(@class,'checkout-success')]//p//a)[1]");
          	    System.out.println(order);
+         	    
+         	   Common.assertionCheckwithReport(order.contains("DRYUSPRE"), "validating the order confirmation page",
+       				expectedResult, "order placed successfully", "unable to place the order");
 		}
 		else
 		{
