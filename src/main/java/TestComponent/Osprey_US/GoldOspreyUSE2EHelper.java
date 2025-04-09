ad655dd56b9828a471e27b38101f0c5cca9163d3
Rafeedha-MD <rafeedha1111@gmail.com>
2025-04-09 12:12:06 +0530

    Osprey US E2E Helper Locater updated

--- src/main/java/TestComponent/Osprey_US/GoldOspreyUSE2EHelper.java
+++ src/main/java/TestComponent/Osprey_US/GoldOspreyUSE2EHelper.java
@@ -10257,9 +10257,9 @@ public void Continue_Shopping() {
 					Common.clickElement("xpath", "//span[text()='Gift Cards']");
 				}
 				
-				Sync.waitElementPresent("xpath", "//img[contains(@itemprop ,'image')]");
+				Sync.waitElementPresent("xpath", "//img[@loading='eager']");
 				List<WebElement> webelementslist = Common.findElements("xpath",
-						"//img[contains(@itemprop ,'image')]");
+						"//img[@loading='eager']");
 
 				String s = webelementslist.get(i).getAttribute("src");
 				System.out.println(s);
