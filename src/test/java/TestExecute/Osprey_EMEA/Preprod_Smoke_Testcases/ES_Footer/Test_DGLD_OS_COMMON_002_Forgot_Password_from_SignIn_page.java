05a0011749476dfb169284012e4708aacce9ef4e
avayugundla <avayugundla@CPC-avayu-3AUJR.helenoftroy.com>
2025-04-09 20:06:25 +0530

    4 new testcases for ES osprey emea store

---
+++ src/test/java/TestExecute/Osprey_EMEA/Preprod_Smoke_Testcases/ES_Footer/Test_DGLD_OS_COMMON_002_Forgot_Password_from_SignIn_page.java
@@ -0,0 +1,47 @@
+package TestExecute.Osprey_EMEA.Preprod_Smoke_Testcases.ES_Footer;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import TestComponent.Osprey_EMEA.OspreyEMEA_HYVA;
+import TestLib.Common;
+import TestLib.Login;
+
+public class Test_DGLD_OS_COMMON_002_Forgot_Password_from_SignIn_page {
+
+	String datafile = "Osprey_EMEA//GoldOspreyemea.xlsx";
+	OspreyEMEA_HYVA Osprey_ReEu = new OspreyEMEA_HYVA(datafile,"CreateAccount");
+
+	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
+	public void Verifying_the_Forgot_Password_Funtionality () throws Exception {
+
+		try {
+        Osprey_ReEu.verifingHomePage();
+        Osprey_ReEu.click_singinButton();
+        Osprey_ReEu.Forgot_password("Account");
+        
+		} catch (Exception e) {
+
+			Assert.fail(e.getMessage(), e);
+		}
+	}
+
+
+	@AfterTest
+	public void clearBrowser() {
+		Common.closeAll();
+
+	}
+
+	@BeforeTest
+	public void startTest() throws Exception {
+		String url = "https://mcloud-na-preprod.osprey.com/es/";
+		System.setProperty("url", url);
+        Login.signIn();
+        
+
+	}
+
+}
