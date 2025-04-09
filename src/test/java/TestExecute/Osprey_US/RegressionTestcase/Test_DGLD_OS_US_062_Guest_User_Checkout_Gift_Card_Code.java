813a610002586a2b8797d53dad5dcc8233f4a848
rmohammad <rmohammad@CPC-rmoha-WQJTL.helenoftroy.com>
2025-04-09 09:23:26 +0000

    OSPREY US-Test_DGLD_OS_US_062_Guest_User_Checkout_Gift_Card_Code

--- src/test/java/TestExecute/Osprey_US/RegressionTestcase/Test_DGLD_OS_US_062_Guest_User_Checkout_Gift_Card_Code.java
+++ src/test/java/TestExecute/Osprey_US/RegressionTestcase/Test_DGLD_OS_US_062_Guest_User_Checkout_Gift_Card_Code.java
@@ -27,7 +27,8 @@ public class Test_DGLD_OS_US_062_Guest_User_Checkout_Gift_Card_Code {
         Osprey_ReEu.clickSubmitbutton_Shippingpage();
         Osprey_ReEu.invalid_Gift_card("Giftcard");         
         Osprey_ReEu.Gift_card("Giftcard");
-        Osprey_ReEu.updatePaymentAndSubmitOrder("CCAmexcard");
+       // Osprey_ReEu.updatePaymentAndSubmitOrder("CCAmexcard");
+        Osprey_ReEu.giftCardSubmitOrder();
         
 		} catch (Exception e) {
 
