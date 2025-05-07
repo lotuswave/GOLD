4b8c4a20cb34f96f64d73a208bfd578235ed8ed8
anjali <anjali@Anjali-Valmiki>
2025-05-07 12:17:05 +0530

    Osprey_US : TEST_DGLD_OSP_US_BCST_001 Testcase changes

--- src/test/java/TestExecute/Osprey_US/Preprod_SmokeTestcases/TEST_DGLD_OSP_US_BCST_001_Create_Account_Funtionality.java
+++ src/test/java/TestExecute/Osprey_US/Preprod_SmokeTestcases/TEST_DGLD_OSP_US_BCST_001_Create_Account_Funtionality.java
@@ -22,8 +22,8 @@ public class TEST_DGLD_OSP_US_BCST_001_Create_Account_Funtionality {
 			Osprey_ReEu.click_Createaccount();
 			Osprey_ReEu.createaccount_verfication("Invalid Email");
 			Osprey_ReEu.create_account("Create Account");
-			Osprey_ReEu.Account_page_Validation("Account validation");
-			Osprey_ReEu.signout();
+//			Osprey_ReEu.Account_page_Validation("Account validation");
+//			Osprey_ReEu.signout();
 
 		} catch (Exception e) {
 
