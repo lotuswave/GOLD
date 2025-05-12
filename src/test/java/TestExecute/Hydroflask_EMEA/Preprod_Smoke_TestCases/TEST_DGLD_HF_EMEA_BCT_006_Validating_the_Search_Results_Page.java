454cfd8ed64d570f4906ca89ccc9a8fc4290bd15
vnarra <vnarra@CPC-vnarr-42JDJ.helenoftroy.com>
2025-05-12 16:30:25 +0530

    Added Hydroflask_EMEA project code-related folders and add smoke tests

---
+++ src/test/java/TestExecute/Hydroflask_EMEA/Preprod_Smoke_TestCases/TEST_DGLD_HF_EMEA_BCT_006_Validating_the_Search_Results_Page.java
@@ -0,0 +1,51 @@
+package TestExecute.Hydroflask_EMEA.Preprod_Smoke_TestCases;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import TestComponent.Hydroflask.GoldHydroHyvaHelper;
+import TestComponent.Hydroflask_EMEA.GoldHydro_EMEA_Helper;
+import TestLib.Common;
+import TestLib.Login;
+
+public class TEST_DGLD_HF_EMEA_BCT_006_Validating_the_Search_Results_Page {
+
+	String datafile = "Hydroflask_EMEA//GoldHydroEMEA_TestData.xlsx";
+	GoldHydro_EMEA_Helper Hydro = new GoldHydro_EMEA_Helper(datafile,"DataSet");
+
+	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
+	public void Validate_Search_Results_Page () throws Exception {
+
+		try {
+			Hydro.verifingHomePage();
+			Hydro.Validating_search("aaabbcc");
+			Hydro.search_results("aaabbcc");
+			Hydro.popular_searches();
+			//Hydro.carousel();
+			Hydro.search_product("Product");
+			
+			
+
+		} catch (Exception e) {
+
+			Assert.fail(e.getMessage(), e);
+		}
+	}
+
+	@AfterTest
+	public void clearBrowser() {
+		Common.closeAll();
+
+	}
+
+	@BeforeTest
+	public void startTest() throws Exception {
+		System.setProperty("configFile", "Hydroflask_EMEA\\config.properties");
+		Login.signIn();
+		Hydro.close_add();
+        Hydro.acceptPrivacy();
+	}
+
+}
---
+++ src/test/java/TestExecute/Hydroflask_EMEA/Preprod_Smoke_TestCases/TEST_DGLD_HF_EMEA_BCT_013_Guest_user_Checkout_Klarna_Payment.java
@@ -0,0 +1,53 @@
+package TestExecute.Hydroflask_EMEA.Preprod_Smoke_TestCases;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import TestComponent.Hydroflask.GoldHydroHyvaHelper;
+import TestComponent.Hydroflask_EMEA.GoldHydro_EMEA_Helper;
+import TestLib.Common;
+import TestLib.Login;
+
+public class TEST_DGLD_HF_EMEA_BCT_013_Guest_user_Checkout_Klarna_Payment {
+
+	String datafile = "Hydroflask_EMEA//GoldHydroEMEA_TestData.xlsx";
+	GoldHydro_EMEA_Helper Hydro = new GoldHydro_EMEA_Helper(datafile,"DataSet");
+
+	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
+	public void Validate_Guest_user_Checkout_Klarna_Payment() throws Exception {
+
+		try {
+			Hydro.verifingHomePage();
+			Hydro.search_product("Product");   
+			Hydro.addtocart("Product");
+			Hydro.minicart_Checkout();
+			Hydro.addDeliveryAddress_Guestuser("AccountDetails");
+            Hydro.selectshippingaddress("GroundShipping method");
+            Hydro.clickSubmitbutton_Shippingpage();
+			Hydro.Guest_Kalrna_Payment("Klarna Visa Payment");
+          
+            
+		} catch (Exception e) {
+
+			Assert.fail(e.getMessage(), e);
+		}
+	}
+
+	@AfterTest
+	public void clearBrowser() {
+		Common.closeAll();
+
+	}
+
+	@BeforeTest
+	public void startTest() throws Exception {
+		System.setProperty("configFile", "Hydroflask_EMEA\\config.properties");
+       Login.signIn();
+       Hydro.close_add();
+       Hydro.acceptPrivacy();
+
+	}
+
+}
