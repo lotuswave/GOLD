a6c8749710bc96aad756977ee072c0abbf93d7cc
anjali <anjali@Anjali-Valmiki>
2025-05-16 14:41:01 +0530

    Osprey_US : Test_DGLD_OSP_US_BCST_031_Validating_the_shopping_cart_page(minicart checkout button tagname changes)

--- src/main/java/TestComponent/Osprey_US/GoldOspreyUSHyvaHelper.java
+++ src/main/java/TestComponent/Osprey_US/GoldOspreyUSHyvaHelper.java
@@ -3007,8 +3007,8 @@ public void Addtocart_From_MyFavorites(String Dataset) {
 			Sync.waitElementPresent("css", "span[x-text='totalCartAmount']");
 			String minicart = Common.findElement("css", "span[x-text='totalCartAmount']").getText();
 			System.out.println(minicart);
-			Sync.waitElementPresent(30, "css", "a[class*='inline-flex btn btn-primary text']");
-			Common.clickElement("css", "a[class*='inline-flex btn btn-primary text']");
+			Sync.waitElementPresent(30, "css", "button[class*='inline-flex btn btn-primary text']");
+			Common.clickElement("css", "button[class*='inline-flex btn btn-primary text']");
 //			Sync.waitPageLoad();
 			Thread.sleep(1000);
 //			Sync.waitElementPresent(30, "xpath", "//strong[@role='heading']");
