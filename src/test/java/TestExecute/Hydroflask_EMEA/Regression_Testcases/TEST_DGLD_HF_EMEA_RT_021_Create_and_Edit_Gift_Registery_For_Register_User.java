87bbcbea52ce517138579b70aa12fb7b9aa8c266
anjali <anjali@Anjali-Valmiki>
2025-05-19 16:09:47 +0530

    HYF_EMEA : TEST_DGLD_HF_EMEA_RT_024_WebPageLinkValidation(updated testdata and added new tetscase for hydroflask emea)

---
+++ src/test/java/TestExecute/Hydroflask_EMEA/Regression_Testcases/TEST_DGLD_HF_EMEA_RT_021_Create_and_Edit_Gift_Registery_For_Register_User.java
@@ -0,0 +1,48 @@
+package TestExecute.Hydroflask_EMEA.Regression_Testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import TestComponent.Hydroflask_EMEA.GoldHydro_EMEA_Helper;
+import TestLib.Login;
+
+public class TEST_DGLD_HF_EMEA_RT_021_Create_and_Edit_Gift_Registery_For_Register_User {
+
+	String datafile = "Hydroflask_EMEA//GoldHydroEMEA_TestData.xlsx";
+	GoldHydro_EMEA_Helper Hydro = new GoldHydro_EMEA_Helper(datafile,"Sheet1");
+	
+
+	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
+	public void Validating_Create_and_Edit_Gift_Registery_For_Register_User () throws Exception {
+
+		try {
+			
+			Hydro.verifingHomePage();
+			Hydro.click_singinButton();
+			Hydro.login_Hydroflask("Giftaccount");
+			Hydro.giftCreation("Birthday");
+			Hydro.edit_gift("AccountDetails");
+      
+		} catch (Exception e) {
+
+			Assert.fail(e.getMessage(), e);
+		}
+	}
+
+	@AfterTest
+	public void clearBrowser() {
+		//Common.closeAll();
+
+	}
+
+	@BeforeTest
+	public void startTest() throws Exception {
+		System.setProperty("configFile", "Hydroflask_EMEA\\config.properties");
+		Login.signIn();
+		Hydro.close_add();
+        Hydro.acceptPrivacy();
+	}
+
+}
---
+++ src/test/java/TestExecute/Hydroflask_EMEA/Regression_Testcases/TEST_DGLD_HF_EMEA_RT_024_WebPageLinkValidation.java
@@ -0,0 +1,46 @@
+package TestExecute.Hydroflask_EMEA.Regression_Testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import TestComponent.Hydroflask_EMEA.GoldHydro_EMEA_Helper;
+import TestLib.Common;
+import TestLib.Login;
+
+public class TEST_DGLD_HF_EMEA_RT_024_WebPageLinkValidation {
+
+	String datafile = "Hydroflask_EMEA//GoldHydroEMEA_TestData.xlsx";
+	GoldHydro_EMEA_Helper Hydro = new GoldHydro_EMEA_Helper(datafile,"weblinks");
+	
+
+	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
+	public void Validating_WebPageLinkValidation () throws Exception {
+
+		try {
+			
+			Hydro.verifingHomePage();
+			Hydro.webpagelinks_validation("Links");
+      
+		} catch (Exception e) {
+
+			Assert.fail(e.getMessage(), e);
+		}
+	}
+
+	@AfterTest
+	public void clearBrowser() {
+		Common.closeAll();
+
+	}
+
+	@BeforeTest
+	public void startTest() throws Exception {
+		System.setProperty("configFile", "Hydroflask_EMEA\\config.properties");
+		Login.signIn();
+		Hydro.close_add();
+        Hydro.acceptPrivacy();
+	}
+
+}
