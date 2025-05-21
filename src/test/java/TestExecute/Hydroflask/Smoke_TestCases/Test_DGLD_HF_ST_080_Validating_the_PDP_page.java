57a3bc53af514f6fae977ab3f24641cd2a9f8f40
spanem <spanem@CPC-spane-J7PSO.helenoftroy.com>
2025-05-21 19:02:18 +0530

    hyf - Prod and preprod smoke test cases updated

---
+++ src/test/java/TestExecute/Hydroflask/Smoke_TestCases/Test_DGLD_HF_ST_080_Validating_the_PDP_page.java
@@ -0,0 +1,60 @@
+package TestExecute.Hydroflask.Smoke_TestCases;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import TestComponent.Hydroflask.GoldHydroHyvaHelper;
+import TestLib.Common;
+import TestLib.Login;
+
+public class Test_DGLD_HF_ST_080_Validating_the_PDP_page {
+	String datafile = "Hydroflask\\GoldHydroTestData.xlsx";	
+	GoldHydroHyvaHelper Hydro = new GoldHydroHyvaHelper(datafile, "DataSet");
+	//GoldHydroHyvaHelper Hydro = new GoldHydroHyvaHelper(datafile, "PDP");
+	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
+	public void Validating_the_PDP_page () throws Exception {
+
+		try {
+			Hydro.verifingHomePage();
+			Hydro.search_product("Product"); 	
+			Hydro.Configurableproduct_addtocart_pdppage("Product");
+			
+			if(Common.getCurrentURL().contains("https://www.hydroflask.com/")) {
+			Hydro.Gift_cards("Hydro Gift Card");
+			Hydro.Card_Value("price");
+			Hydro.minicart_Checkout();
+			
+			}
+		}
+		catch (Exception e) {
+			
+			Assert.fail(e.getMessage(), e);
+		} 
+	}
+	
+	
+	
+	@AfterTest
+	public void clearBrowser()
+	{
+		Common.closeAll();
+
+	}
+	
+	
+	@BeforeTest
+	  public void startTest() throws Exception {
+		System.setProperty("configFile", "Hydroflask\\config.properties");
+		  Login.signIn();
+		  Hydro.close_add();
+	  Hydro.acceptPrivacy();
+		  
+	  }
+
+	
+
+
+
+}
