da70354145c25d831b9d277f28439c8fc857d749
vnarra <vnarra@CPC-vnarr-42JDJ.helenoftroy.com>
2025-05-29 13:34:13 +0530

    HF-EU: Moke testcases created for other stores

---
+++ src/test/java/TestExecute/Hydroflask_EMEA/Preprod_Smoke_TestCases/EU_Smoke/TEST_DGLD_HF_EMEA_EU_BCT_005_Validating_the_PDP_page.java
@@ -0,0 +1,56 @@
+package TestExecute.Hydroflask_EMEA.Preprod_Smoke_TestCases.EU_Smoke;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import TestComponent.Hydroflask_EMEA.GoldHydro_EMEA_Helper;
+import TestLib.Common;
+import TestLib.Login;
+
+public class TEST_DGLD_HF_EMEA_EU_BCT_005_Validating_the_PDP_page {
+	
+	String datafile = "Hydroflask_EMEA//GoldHydroEMEA_TestData.xlsx";
+	GoldHydro_EMEA_Helper Hydro = new GoldHydro_EMEA_Helper(datafile, "PDP");
+	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
+	public void Validating_the_PDP_page () throws Exception {
+
+		try {
+			Hydro.verifingHomePage();
+			Hydro.search_product("Product"); 	
+			Hydro.Configurableproduct_addtocart_pdppage("Product");
+			
+
+		
+		}
+		catch (Exception e) {
+			
+			Assert.fail(e.getMessage(), e);
+		} 
+	}
+	
+	
+	
+	@AfterTest
+	public void clearBrowser()
+	{
+		Common.closeAll();
+
+	}
+	
+	
+	@BeforeTest
+	public void startTest() throws Exception {
+		System.setProperty("configFile", "Hydroflask_EMEA\\config.properties");
+        Login.signIn();
+        Hydro.close_add();
+        Hydro.acceptPrivacy();
+
+	}
+
+	
+
+
+
+}
