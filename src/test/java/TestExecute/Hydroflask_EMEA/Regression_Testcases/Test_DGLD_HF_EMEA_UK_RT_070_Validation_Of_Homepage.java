e0af530d42be137f3c3f2141aecd3fc52b859b89
vnarra <vnarra@CPC-vnarr-42JDJ.helenoftroy.com>
2025-07-08 15:04:54 +0530

    HF-EMEA new testcases added

---
+++ src/test/java/TestExecute/Hydroflask_EMEA/Regression_Testcases/Test_DGLD_HF_EMEA_UK_RT_070_Validation_Of_Homepage.java
@@ -0,0 +1,51 @@
+package TestExecute.Hydroflask_EMEA.Regression_Testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import TestComponent.Hydroflask.GoldHydroHyvaHelper;
+import TestComponent.Hydroflask_EMEA.GoldHydro_EMEA_Helper;
+import TestLib.Common;
+import TestLib.Login;
+
+public class Test_DGLD_HF_EMEA_UK_RT_070_Validation_Of_Homepage {
+
+	String datafile = "Hydroflask_EMEA//GoldHydroEMEA_TestData.xlsx";
+	GoldHydro_EMEA_Helper Hydro = new GoldHydro_EMEA_Helper(datafile,"DataSet");
+
+	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
+	public void Validate_Homepage_Elements_Displayed  () throws Exception {
+
+		try {
+			Hydro.verifingHomePage();
+			Hydro.Store_Logo_Validation();
+			Hydro.Hero_Banner_Validation();
+			Hydro.CatogeryORproduct_Tile_Validation();
+			Hydro.CatogeryORproduct_Slider_Validation();
+			Hydro.Promo_Block_Validation();
+//			Hydro.Marketing_Flyout_Validation();
+			
+
+		} catch (Exception e) {
+
+			Assert.fail(e.getMessage(), e);
+		}
+	}
+
+	@AfterTest
+	public void clearBrowser() {
+		Common.closeAll();
+
+	}
+
+	@BeforeTest
+	public void startTest() throws Exception {
+		System.setProperty("configFile", "Hydroflask_EMEA\\config.properties");
+		Login.signIn();
+		Hydro.close_add();
+        Hydro.acceptPrivacy();
+	}
+
+}
