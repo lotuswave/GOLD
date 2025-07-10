facf337741543b88e70efeebea6e66af234f48d8
vnarra <vnarra@CPC-vnarr-42JDJ.helenoftroy.com>
2025-07-10 17:12:03 +0530

    HF-EMEA : Added new testcases 003 and 004

---
+++ src/test/java/TestExecute/Hydroflask_EMEA/Regression_Testcases/TEST_DGLD_HF_EMEA_RT_003_Validation_ShippingAddress_form_for_GuestUser.java
@@ -0,0 +1,49 @@
+package TestExecute.Hydroflask_EMEA.Regression_Testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import TestComponent.Hydroflask.GoldHydroHyvaHelper;
+import TestComponent.Hydroflask_EMEA.GoldHydro_EMEA_Helper;
+import TestLib.Common;
+import TestLib.Login;
+
+public class TEST_DGLD_HF_EMEA_RT_003_Validation_ShippingAddress_form_for_GuestUser {
+
+	String datafile = "Hydroflask_EMEA//GoldHydroEMEA_TestData.xlsx";
+	GoldHydro_EMEA_Helper Hydro = new GoldHydro_EMEA_Helper(datafile,"DataSet");
+
+	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
+    public void Validation_ShippingAddress_form_for_GuestUesr_with_Errormessages() throws Exception {
+        try {
+            Hydro.verifingHomePage();
+            Hydro.search_product("Product");      
+            Hydro.addtocart("Product");                    
+            Hydro.minicart_Checkout();
+            Hydro.addDeliveryAddress_Gustuser("InvalidAddress");
+            Hydro.validatingErrormessageShippingpage_negative();
+       
+        
+        } catch (Exception e) {
+
+			Assert.fail(e.getMessage(), e);
+		}
+	}
+
+	@AfterTest
+	public void clearBrowser() {
+		Common.closeAll();
+
+	}
+
+	@BeforeTest
+	public void startTest() throws Exception {
+		System.setProperty("configFile", "Hydroflask_EMEA\\config.properties");
+		Login.signIn();
+		Hydro.close_add();
+        Hydro.acceptPrivacy();
+	}
+
+}
---
+++ src/test/java/TestExecute/Hydroflask_EMEA/Regression_Testcases/TEST_DGLD_HF_EMEA_RT_004_Validation_ShippingAddress_form_for_RegisterUesr.java
@@ -0,0 +1,53 @@
+package TestExecute.Hydroflask_EMEA.Regression_Testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import TestComponent.Hydroflask.GoldHydroHyvaHelper;
+import TestComponent.Hydroflask_EMEA.GoldHydro_EMEA_Helper;
+import TestLib.Common;
+import TestLib.Login;
+
+public class TEST_DGLD_HF_EMEA_RT_004_Validation_ShippingAddress_form_for_RegisterUesr {
+
+	String datafile = "Hydroflask_EMEA//GoldHydroEMEA_TestData.xlsx";
+	GoldHydro_EMEA_Helper Hydro = new GoldHydro_EMEA_Helper(datafile,"DataSet");
+
+	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
+	public void Validate_registeredUserCheckoutwith_invalid_CC_Credentials () throws Exception {
+
+		try {
+			Hydro.verifingHomePage();
+			Hydro.click_singinButton();
+			Hydro.login_Hydroflask("AccountDetails");
+			Hydro.search_product("Product");      
+			Hydro.addtocart("Product");                    
+			Hydro.minicart_Checkout();
+			Hydro.RegaddDeliveryAddress("AccountDetails");
+            Hydro.selectshippingaddress("GroundShipping method");
+            Hydro.clickSubmitbutton_Shippingpage();
+			Hydro.updtePayementcrditcard_WithInvalidData("invlidPaymentDetails");
+
+		} catch (Exception e) {
+
+			Assert.fail(e.getMessage(), e);
+		}
+	}
+
+	@AfterTest
+	public void clearBrowser() {
+		Common.closeAll();
+
+	}
+
+	@BeforeTest
+	public void startTest() throws Exception {
+		System.setProperty("configFile", "Hydroflask_EMEA\\config.properties");
+		Login.signIn();
+		Hydro.close_add();
+        Hydro.acceptPrivacy();
+	}
+
+}
