fc15820de0dd57cc6665511b520c6d26c87a99c9
vnarra <vnarra@CPC-vnarr-42JDJ.helenoftroy.com>
2025-07-18 14:08:37 +0530

    Test_DGLD_HF_EMEA_RT_044_Validate_Blog_page added new testcase

---
+++ src/test/java/TestExecute/Hydroflask_EMEA/Regression_Testcases/Test_DGLD_HF_EMEA_RT_044_Validate_Blog_page.java
@@ -0,0 +1,47 @@
+package TestExecute.Hydroflask_EMEA.Regression_Testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import TestComponent.Hydroflask.GoldHydroHyvaHelper;
+import TestComponent.Hydroflask_EMEA.GoldHydro_EMEA_Helper;
+import TestLib.Common;
+import TestLib.Login;
+
+public class Test_DGLD_HF_EMEA_RT_044_Validate_Blog_page {
+	
+	String datafile = "Hydroflask_EMEA//GoldHydroEMEA_TestData.xlsx";
+	GoldHydro_EMEA_Helper Hydro = new GoldHydro_EMEA_Helper(datafile,"Blog");
+
+	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
+	public void Validating_HydroFlask_Blogs_Page () throws Exception {
+
+		try {
+			Hydro.verifingHomePage();
+			Hydro.Blog_Page("Blog");
+			Hydro.blog_page();
+			
+		} catch (Exception e) {
+
+			Assert.fail(e.getMessage(), e);
+		}
+	}
+
+	@AfterTest
+	public void clearBrowser() {
+		Common.closeAll();
+
+	}
+
+	@BeforeTest
+	public void startTest() throws Exception {
+		System.setProperty("configFile", "Hydroflask_EMEA\\config.properties");
+		Login.signIn();
+		Hydro.close_add();
+        Hydro.acceptPrivacy();
+
+	}
+
+}
