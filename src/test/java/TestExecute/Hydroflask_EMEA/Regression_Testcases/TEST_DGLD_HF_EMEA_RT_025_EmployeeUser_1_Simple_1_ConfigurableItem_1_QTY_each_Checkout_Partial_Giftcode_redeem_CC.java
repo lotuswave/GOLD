9fb476af81bf052f3cf9763f8bc54d6159e67638
avayugundla <avayugundla@CPC-avayu-3AUJR.helenoftroy.com>
2025-07-18 16:03:54 +0530

    TEST_DGLD_HF_EMEA_RT_025_EmployeeUser_1_Simple_1_ConfigurableItem_1_QTY_each_Checkout_Partial_Giftcode_redeem_CC
    New testcase for Hydroflask emea store

---
+++ src/test/java/TestExecute/Hydroflask_EMEA/Regression_Testcases/TEST_DGLD_HF_EMEA_RT_025_EmployeeUser_1_Simple_1_ConfigurableItem_1_QTY_each_Checkout_Partial_Giftcode_redeem_CC.java
@@ -0,0 +1,56 @@
+package TestExecute.Hydroflask_EMEA.Regression_Testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import TestComponent.Hydroflask_EMEA.GoldHydro_EMEA_Helper;
+import TestLib.Common;
+import TestLib.Login;
+
+public class TEST_DGLD_HF_EMEA_RT_025_EmployeeUser_1_Simple_1_ConfigurableItem_1_QTY_each_Checkout_Partial_Giftcode_redeem_CC {
+
+	String datafile = "Hydroflask_EMEA//GoldHydroEMEA_TestData.xlsx";
+	GoldHydro_EMEA_Helper Hydro = new GoldHydro_EMEA_Helper(datafile,"DataSet");
+
+	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
+	public void Validating_EmployeeUser_1_Simple_1_ConfigurableItem_1_QTY_each_Checkout_Partial_Giftcode_redeem_CC () throws Exception {
+		
+		try {
+			Hydro.verifingHomePage();
+			Hydro.click_singinButton();
+			Hydro.login_Hydroflask("Employee_id");
+			Hydro.search_product("Product");      
+			Hydro.addtocart("Product"); 
+			Hydro.employee_discount();
+			Hydro.bottles_headerlinks("Bottles & Drinkware"); 
+			Hydro.Configurable_addtocart_pdp("Product");
+			Hydro.employee_discount();
+			Hydro.RegaddDeliveryAddress("Employee_id");
+            Hydro.selectshippingaddress("GroundShipping method");
+            Hydro.Gift_card("Giftcard_Partial_4");
+            Hydro.updatePaymentAndSubmitOrder("PaymentDetails");
+
+		} catch (Exception e) {
+
+			Assert.fail(e.getMessage(), e);
+		}
+	}
+
+
+	@AfterTest
+	public void clearBrowser() {
+		Common.closeAll();
+
+	}
+
+	@BeforeTest
+	public void startTest() throws Exception {
+		System.setProperty("configFile", "Hydroflask_EMEA\\config.properties");
+		Login.signIn();
+		Hydro.close_add();
+        Hydro.acceptPrivacy();
+	}
+
+}
