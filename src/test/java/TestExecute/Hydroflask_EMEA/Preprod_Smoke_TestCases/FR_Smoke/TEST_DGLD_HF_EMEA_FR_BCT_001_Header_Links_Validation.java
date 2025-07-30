067f1310363f4ba41bd26fc5e1143188024a811e
avayugundla <avayugundla@CPC-avayu-3AUJR.helenoftroy.com>
2025-07-30 17:17:08 +0530

    TEST_DGLD_HF_EMEA_FR_BCT_001_Header_Links_Validation Fr Header links for Hydroflask emea

--- src/test/java/TestExecute/Hydroflask_EMEA/Preprod_Smoke_TestCases/FR_Smoke/TEST_DGLD_HF_EMEA_FR_BCT_001_Header_Links_Validation.java
+++ src/test/java/TestExecute/Hydroflask_EMEA/Preprod_Smoke_TestCases/FR_Smoke/TEST_DGLD_HF_EMEA_FR_BCT_001_Header_Links_Validation.java
@@ -13,29 +13,23 @@ import TestLib.Login;
 public class TEST_DGLD_HF_EMEA_FR_BCT_001_Header_Links_Validation {
 
 	String datafile = "Hydroflask_EMEA//GoldHydroEMEA_TestData.xlsx";
-	GoldHydro_EMEA_Helper Hydro = new GoldHydro_EMEA_Helper(datafile,"HeaderLinks");
+	GoldHydro_EMEA_Helper Hydro = new GoldHydro_EMEA_Helper(datafile,"HeaderLinks_FR");
 
 	@Test(retryAnalyzer = Utilities.RetryAnalyzer.class)
 	public void Validate_Header_Links_Validation () throws Exception {
 
 		try {
 			Hydro.verifingHomePage();
-			Hydro.New_Color_Destination("Colors");	
-	//	    Hydro.travel_Bottles_validation("travel Bottles");   //add zero product 
-		    Hydro.Travel_Tumbler_validation("Travel Tumbler");			  
-	//		Hydro.shop_by_Color_validation("Shop by Color");          
-            Hydro.bottles_validation("Bottle and drinkware");	
-        	Hydro.Coolers_validation("Coolers");                                 
-    //     	Hydro.kitchenware_validation("kitchenware");          	
-            Hydro.Accessories_validation("Accessories");           
-    //        Hydro.Collections_validation("Collections");            //working fine needsto commit  and add one zero product
+			Hydro.New_Color_Destination("Colors");	          
+            Hydro.bottles_validation("Bottle and drinkware");	   
+        	Hydro.Coolers_validation("Coolers");                   	
+            Hydro.Accessories_validation("Accessories"); 
 			Hydro.Explore_Validation("Explore Links");                
 			Hydro.featured_validation("Featured");                       
-			Hydro.New_Color_Destination("Colors");
 			Hydro.featured_Shopby_Collections("Shopby Collections");
-			
-//			Hydro.Customize_validation("Customize");			
-//			Hydro.Holiday_shop_validation("Holiday Sale"); 
+			Hydro.featured_Shopby_Activity("Shopby Activity");
+			Hydro.Shop_Shopall("Shop All");
+			Hydro.Featured_ShopAll("Featured shopall");
 			
 			} catch (Exception e) {
 
@@ -52,7 +46,7 @@ public class TEST_DGLD_HF_EMEA_FR_BCT_001_Header_Links_Validation {
 
 	@BeforeTest
 	public void startTest() throws Exception {
-		String url="https://mcloud-na-stage4.hydroflask.com/fr";
+		String url="https://www..hydroflask.com/fr";
 		System.setProperty("url", url);
         Login.signIn();
         Hydro.close_add();
